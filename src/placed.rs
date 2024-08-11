e3c9f4e6bbd3cfdb28121a653d4b6a176daf0ccf
abscosmos <joshua@dinoslice.com>
2024-08-11 10:38:55 -0500

    Create Placed<T>

---
+++ src/placed.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+pub struct Placed<T> {
+    pub base: T,
+    pub octave: i16,
+}
+
+impl<T> Placed<T> {
+    pub fn to_base(self) -> T {
+        self.base
+    }
+
+    pub fn as_base(&self) -> &T {
+        &self.base
+    }
+}
