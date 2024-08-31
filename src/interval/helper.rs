67b13b96660e8a0588a5bdd9f031f36f3a8bed14
abscosmos <joshua@dinoslice.com>
2024-08-31 09:54:55 -0500

    Helper macro to create interval

--- src/interval/helper.rs
+++ src/interval/helper.rs
@@ -28,3 +28,13 @@ macro_rules! intervals_vec {
         ]
     };
 }
+
+#[macro_export]
+macro_rules! interval {
+    ($quality:ident $size:ident) => {
+        crate::interval::Interval::from_quality_and_size(
+            crate::interval::quality::IntervalQuality::$quality,
+            crate::interval::size::IntervalSize::$size
+        ).expect("interval to be valid")
+    };
+}
