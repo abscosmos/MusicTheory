6fee92aa58a5c599b777d3ab6d5af742fe0acf48
abscosmos <joshua@dinoslice.com>
2024-11-29 18:34:15 -0600

    Fix interval helper macros

--- src/interval/helper.rs
+++ src/interval/helper.rs
@@ -3,38 +3,29 @@ macro_rules! intervals {
     ($( $quality:ident $size:ident ),*) => {
         [
             $(
-                crate::Interval::from_quality_and_size(
-                    crate::interval::quality::IntervalQuality::$quality,
-                    crate::interval::size::IntervalSize::$size
-                ).expect("interval to be valid")
+                $crate::interval!($quality $size)
             ),*
         ]
     };
     (vec <- $( $quality:ident $size:ident ),*) => {
-        ivls_slice!($( $quality $size ),*).to_vec()
+        $crate::intervals!($( $quality $size ),*).to_vec()
     };
 }
 
 #[macro_export]
 macro_rules! intervals_vec {
     ($( $quality:ident $size:ident ),*) => {
-        vec![
-            $(
-                crate::interval::Interval::from_quality_and_size(
-                    crate::interval::quality::IntervalQuality::$quality,
-                    crate::interval::size::IntervalSize::$size
-                ).expect("interval to be valid")
-            ),*
-        ]
+        $crate::intervals!(vec <- $( $quality $size ),*)
     };
 }
 
 #[macro_export]
 macro_rules! interval {
-    ($quality:ident $size:ident) => {
-        crate::interval::Interval::from_quality_and_size(
-            crate::interval::quality::IntervalQuality::$quality,
-            crate::interval::size::IntervalSize::$size
-        ).expect("interval to be valid")
-    };
+    ($quality:ident $size:ident) => {{
+        let quality = $crate::interval::quality::IntervalQuality::$quality;
+        let size = $crate::interval::size::IntervalSize::$size;
+                
+        $crate::Interval::from_quality_and_size(quality, size)
+            .unwrap_or_else(|| panic!("{quality:?} {size:?} isn't a valid interval"))
+    }};
 }
