3cbce04dcf268e3fbd77e1a93f2c692a98d10227
abscosmos <joshua@dinoslice.com>
2024-12-08 22:03:48 -0600

    Finish creating tests for Interval

--- src/interval/mod.rs
+++ src/interval/mod.rs
@@ -183,6 +183,15 @@ mod tests {
     use IntervalQuality as IQ;
     use IntervalNumber as IN;
 
+    // helper fns
+    fn semi(ivl: I) -> i16 {
+        ivl.semitones().0
+    }
+
+    fn ivl(q: IQ, sz: i16) -> I {
+        I::new(q, IN::new(sz).expect("nonzero")).expect("valid interval")
+    }
+
     #[test]
     fn new() {
         for num in 1..25 {
@@ -219,29 +228,35 @@ mod tests {
     }
     
     #[test]
-    fn non_subzero() {
+    fn subzero() {
         assert!(I::strict_non_subzero(IQ::DIMINISHED, IN::UNISON).is_none());
+        assert!(I::new(IQ::DIMINISHED, IN::UNISON).expect("valid interval").inverted().inverted_strict_non_subzero().is_none());
         
         for num in 2..15 {
             let num = IN::new(num).expect("nonzero");
             assert!(I::strict_non_subzero(IQ::DIMINISHED, num).is_some());
+            assert!(I::strict_non_subzero(IQ::DIMINISHED, num).expect("non subzero").inverted_strict_non_subzero().is_some());
         }
 
         let doubly_diminished = IQ::Diminished(NonZeroU16::new(2).expect("nonzero"));
         
         assert!(I::strict_non_subzero(doubly_diminished, IN::UNISON).is_none());
+        assert!(I::new(doubly_diminished, IN::UNISON).expect("valid interval").inverted().inverted_strict_non_subzero().is_none());
         assert!(I::strict_non_subzero(doubly_diminished, IN::SECOND).is_none());
+        assert!(I::new(doubly_diminished, IN::SECOND).expect("valid interval").inverted().inverted_strict_non_subzero().is_none());
 
         for num in 3..15 {
             let num = IN::new(num).expect("nonzero");
             assert!(I::strict_non_subzero(doubly_diminished, num).is_some());
+            assert!(I::strict_non_subzero(doubly_diminished, num).expect("non subzero").inverted_strict_non_subzero().is_some());
         }
+
+        assert!(I::new(IQ::DIMINISHED, IN::UNISON).expect("valid quality").is_subzero());
+        assert!(!I::new(IQ::DIMINISHED, IN::SECOND).expect("valid quality").is_subzero());
     }
     
     #[test]
     fn semitones_constants() {
-        let semi = |ivl: I| ivl.semitones().0;
-        
         assert_eq!(semi(I::PERFECT_UNISON), 0);
         assert_eq!(semi(I::DIMINISHED_SECOND), 0);
         
@@ -317,5 +332,182 @@ mod tests {
         
         assert_eq!(semi(I::PERFECT_FIFTEENTH), 24);
         assert_eq!(semi(I::AUGMENTED_FOURTEENTH), 24);
+    }
+
+    #[test]
+    fn semitones_negative() {
+        assert_eq!(semi(-I::PERFECT_UNISON), 0);
+        assert_eq!(semi(-I::DIMINISHED_SECOND), 0);
+
+        assert_eq!(semi(-I::MINOR_SECOND), -1);
+        assert_eq!(semi(-I::AUGMENTED_UNISON), -1);
+
+        assert_eq!(semi(-I::MAJOR_SEVENTH), -11);
+        assert_eq!(semi(-I::DIMINISHED_OCTAVE), -11);
+
+        assert_eq!(semi(-I::PERFECT_OCTAVE), -12);
+        assert_eq!(semi(-I::AUGMENTED_SEVENTH), -12);
+        assert_eq!(semi(-I::DIMINISHED_NINTH), -12);
+
+        assert_eq!(semi(-I::MAJOR_FOURTEENTH), -23);
+        assert_eq!(semi(-I::DIMINISHED_FIFTEENTH), -23);
+
+        assert_eq!(semi(-I::PERFECT_FIFTEENTH), -24);
+        assert_eq!(semi(-I::AUGMENTED_FOURTEENTH), -24);
+    }
+
+    #[test]
+    fn semitones_aug_dim() {
+        fn dim(adj: u16, sz: i16) -> I {
+            I::new(
+                IQ::Diminished(NonZeroU16::new(adj).expect("nonzero")),
+                IN::new(sz).expect("nonzero")
+            ).expect("valid interval")
+        }
+
+        fn aug(adj: u16, sz: i16) -> I {
+            I::new(
+                IQ::Augmented(NonZeroU16::new(adj).expect("nonzero")),
+                IN::new(sz).expect("nonzero")
+            ).expect("valid interval")
+        }
+
+        assert_eq!(semi(dim(7, -80)), -128);
+        assert_eq!(semi(dim(6, 4)), -1); // subzero
+        assert_eq!(semi(dim(5, -45)), -70);
+        assert_eq!(semi(dim(4, 30)), 45);
+        assert_eq!(semi(dim(3, -75)), -124);
+        assert_eq!(semi(dim(2, 6)), 6);
+
+        assert_eq!(semi(aug(2, -38)), -66);
+        assert_eq!(semi(aug(3, -11)), -20);
+        assert_eq!(semi(aug(4, 59)), 104);
+        assert_eq!(semi(aug(5, 25)), 46);
+        assert_eq!(semi(aug(6, -53)), -95);
+        assert_eq!(semi(aug(7, 34)), 64);
+    }
+
+    #[test]
+    fn semitones_general() {
+        assert_eq!(semi(ivl(IQ::Perfect, -39)), -65);
+        assert_eq!(semi(ivl(IQ::Major, 31)), 52);
+        assert_eq!(semi(ivl(IQ::Minor, -76)), -128);
+        assert_eq!(semi(ivl(IQ::Perfect, 40)), 67);
+        assert_eq!(semi(ivl(IQ::Major, 17)), 28);
+        assert_eq!(semi(ivl(IQ::Minor, -77)), -130);
+        assert_eq!(semi(ivl(IQ::Perfect, -19)), -31);
+        assert_eq!(semi(ivl(IQ::Major, 48)), 81);
+        assert_eq!(semi(ivl(IQ::Minor, 21)), 34);
+    }
+
+    #[test]
+    fn from_semitones() {
+        assert_eq!(
+            (0..=12)
+                .map(|s| Interval::from_semitones_preferred(Semitone(s)))
+                .collect::<Vec<_>>(),
+            [
+                I::PERFECT_UNISON, I::MINOR_SECOND, I::MAJOR_SECOND,
+                I::MINOR_THIRD, I::MAJOR_THIRD, I::PERFECT_FOURTH,
+                I::DIMINISHED_FIFTH, I::PERFECT_FIFTH, I::MINOR_SIXTH,
+                I::MAJOR_SIXTH, I::MINOR_SEVENTH, I::MAJOR_SEVENTH,
+                I::PERFECT_OCTAVE
+            ]
+        );
+
+        assert_eq!(Interval::from_semitones_preferred(Semitone(76)), ivl(IQ::Major, 45));
+        assert_eq!(Interval::from_semitones_preferred(Semitone(21)), ivl(IQ::Major, 13));
+        assert_eq!(Interval::from_semitones_preferred(Semitone(-31)), ivl(IQ::Perfect, -19));
+        assert_eq!(Interval::from_semitones_preferred(Semitone(58)), ivl(IQ::Minor, 35));
+        assert_eq!(Interval::from_semitones_preferred(Semitone(14)), ivl(IQ::Major, 9));
+        assert_eq!(Interval::from_semitones_preferred(Semitone(-27)), ivl(IQ::Minor, -17));
+        assert_eq!(Interval::from_semitones_preferred(Semitone(-17)), ivl(IQ::Perfect, -11));
+        assert_eq!(Interval::from_semitones_preferred(Semitone(16)), ivl(IQ::Major, 10));
+        assert_eq!(Interval::from_semitones_preferred(Semitone(-66)), ivl(IQ::DIMINISHED, -40));
+        assert_eq!(Interval::from_semitones_preferred(Semitone(72)), ivl(IQ::Perfect, 43));
+    }
+
+    #[test]
+    fn to_from_semitones_inverse() {
+        for semis in -75..75 {
+            assert_eq!(semi(I::from_semitones_preferred(Semitone(semis))), semis);
+        }
+    }
+
+    #[test]
+    fn shorthand_display() {
+        assert_eq!(I::PERFECT_FIFTEENTH.shorthand(), "P15");
+        assert_eq!(I::PERFECT_FIFTEENTH.to_string(), "P15");
+    }
+
+    #[test]
+    fn inverted() {
+        assert_eq!(I::PERFECT_UNISON.inverted(), I::PERFECT_UNISON);
+        assert_eq!(I::DIMINISHED_SECOND.inverted(), I::AUGMENTED_SEVENTH);
+        assert_eq!(-I::MINOR_THIRD.inverted(), -I::MAJOR_SIXTH);
+        assert_eq!(I::DIMINISHED_FOURTH.inverted(), I::AUGMENTED_FIFTH);
+        assert_eq!(-I::PERFECT_FIFTH.inverted(), -I::PERFECT_FOURTH);
+        assert_eq!(I::AUGMENTED_SIXTH.inverted(), I::DIMINISHED_THIRD);
+        assert_eq!(I::MAJOR_SEVENTH.inverted(), I::MINOR_SECOND);
+        assert_eq!(-I::DIMINISHED_OCTAVE.inverted(), -I::AUGMENTED_OCTAVE);
+        assert_eq!(I::PERFECT_OCTAVE.inverted(), I::PERFECT_OCTAVE);
+        
+        assert_eq!(-I::MINOR_TENTH.inverted(), -I::MAJOR_THIRTEENTH);
+        assert_eq!(I::AUGMENTED_ELEVENTH.inverted(), I::DIMINISHED_TWELFTH);
+        assert_eq!(I::PERFECT_TWELFTH.inverted(), I::PERFECT_ELEVENTH);
+        assert_eq!(-I::DIMINISHED_THIRTEENTH.inverted(), -I::AUGMENTED_TENTH);
+        assert_eq!(-I::MAJOR_FOURTEENTH.inverted(), -I::MINOR_NINTH);
+        assert_eq!(I::PERFECT_FIFTEENTH.inverted(), I::PERFECT_FIFTEENTH);
+
+        assert_eq!(ivl(IQ::Perfect, -39).inverted(), ivl(IQ::Perfect, -40));
+        assert_eq!(ivl(IQ::Major, 31).inverted(), ivl(IQ::Minor, 34));
+        assert_eq!(ivl(IQ::Minor, -76).inverted(), ivl(IQ::Major, -73));
+        assert_eq!(ivl(IQ::Perfect, 40).inverted(), ivl(IQ::Perfect, 39));
+        assert_eq!(ivl(IQ::Major, 17).inverted(), ivl(IQ::Minor, 20));
+        assert_eq!(ivl(IQ::Minor, -77).inverted(), ivl(IQ::Major, -72));
+        assert_eq!(ivl(IQ::Perfect, -19).inverted(), ivl(IQ::Perfect, -18));
+        assert_eq!(ivl(IQ::Major, 48).inverted(), ivl(IQ::Minor, 45));
+        assert_eq!(ivl(IQ::Minor, 21).inverted(), ivl(IQ::Major, 16));
+    }
+
+    #[test]
+    fn double_inversion() {
+        for &ivl in I::ALL_CONSTS {
+            assert_eq!(ivl.inverted().inverted(), ivl);
+        }
+    }
+
+    #[test]
+    fn direction() {
+        assert!(I::MAJOR_NINTH.is_ascending());
+        assert!(I::DIMINISHED_TWELFTH.is_ascending());
+
+        assert!(!(-I::MINOR_SEVENTH).is_ascending());
+        assert!(!(-I::AUGMENTED_FOURTEENTH).is_ascending());
+
+        assert_eq!(I::MINOR_SEVENTH.with_direction(true), I::MINOR_SEVENTH);
+        assert_eq!(I::MAJOR_SECOND.with_direction(false), -I::MAJOR_SECOND);
+
+        assert_eq!((-I::AUGMENTED_FIFTH).with_direction(true), I::AUGMENTED_FIFTH);
+        assert_eq!((-I::PERFECT_ELEVENTH).with_direction(false), -I::PERFECT_ELEVENTH);
+    }
+
+    #[test]
+    fn eq_ord_enharmonic() {
+        assert!(I::MAJOR_SIXTH.eq_enharmonic(&I::DIMINISHED_SEVENTH));
+        assert!(I::AUGMENTED_THIRTEENTH.eq_enharmonic(&I::MINOR_FOURTEENTH));
+
+        assert!(!I::MINOR_THIRD.eq_enharmonic(&I::DIMINISHED_FOURTH));
+        assert!(!I::PERFECT_TWELFTH.eq_enharmonic(&I::AUGMENTED_TWELFTH));
+
+        assert_eq!(I::AUGMENTED_FOURTH.cmp_enharmonic(&I::PERFECT_FIFTH), Ordering::Less);
+        assert_eq!(I::MAJOR_NINTH.cmp_enharmonic(&I::DIMINISHED_TENTH), Ordering::Equal);
+        assert_eq!(I::PERFECT_FIFTEENTH.cmp_enharmonic(&I::DIMINISHED_FIFTEENTH), Ordering::Greater);
+    }
+
+    #[test]
+    fn neg() {
+        assert_eq!((-I::DIMINISHED_FOURTEENTH).shorthand(), "d-14");
+        assert_eq!(-(-I::MAJOR_SEVENTH), I::MAJOR_SEVENTH);
     }
 }
