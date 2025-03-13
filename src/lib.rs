375117064603fcba5e974e654da2b05880b1533e
abscosmos <joshua@dinoslice.com>
2025-03-12 22:31:26 -0500

    Expand TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,21 @@ pub mod letter;
 /*
 TODO:
     - add prelude
-    - add pcset
     - derive Copy where applicable and change &Self -> Self
     - full documentation
     - full tests
     - easy conversion using From for everything applicable
+    - score stuff
+    - tonal parity:
+        - abc notation
+        - scales & scale types
+        - chords
+        - pcsets
+        - keys & modes
+        - progressions & roman numerals
+        - voicings
+        - rhythm, time signatures, duration
+    - music21 parity?
+        - harmony
+        - ...
 */
