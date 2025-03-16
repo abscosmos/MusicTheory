aa76d652b05854028842cd30f85c6da94f5b2576
abscosmos <joshua@dinoslice.com>
2025-03-16 14:06:34 -0500

    Implement HeptatoniaSecunda scale modes (melodic minor modes)

--- src/scales.rs
+++ src/scales.rs
@@ -52,6 +52,53 @@ impl DiatonicMode {
     } 
 }
 
+// TODO: should this be I -> VII with consts for the names?
+#[repr(u8)]
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Ord, PartialOrd, FromRepr)]
+pub enum HeptatoniaSecundaMode {
+    MelodicMinor = 1,
+    DorianFlat2,
+    LydianAugmented,
+    LydianDominant,
+    MixolydianFlat6,
+    HalfDiminished,
+    Altered,
+}
+
+// TODO: abstract into trait?
+impl HeptatoniaSecundaMode {
+    const INTERVALS: [Interval; 7] = [T, S, T, T, T, T, S];
+    
+    pub fn build_from<T: Add<Interval, Output = T> + Clone>(&self, root: T) -> [T; 7] {
+        let mode = self.number() as usize;
+
+        let mut curr = root;
+
+        array::from_fn(|i| {
+            let ret = curr.clone();
+
+            curr = curr.clone() + Self::INTERVALS[(i + mode - 1) % Self::INTERVALS.len()];
+
+            ret
+        })
+    }
+
+    pub fn intervals(&self) -> [Interval; 7] {
+        self.build_from(Interval::PERFECT_UNISON)
+    }
+
+    pub fn number(&self) -> u8 {
+        *self as _
+    }
+
+    pub fn from_number(number: u8) -> Option<Self> {
+        Self::from_repr(number)
+    }
+}
+
+// TODO: MelodicAscendingMinorMode? 
+pub use HeptatoniaSecundaMode as MelodicMinorMode;
+
 #[cfg(test)]
 mod tests {
     use crate::pitch::Pitch;
