5ab532611d3c62d7780b208e425817cd5972f3ad
abscosmos <joshua@dinoslice.com>
2025-03-15 21:19:31 -0500

    Change name of DiatonicMode::notes -> build_from

--- src/scales.rs
+++ src/scales.rs
@@ -1,31 +1,63 @@
 use std::array;
+use std::ops::Add;
 use crate::interval::Interval;
 
-pub trait Scale: Sized {
-    type IntervalSet; 
-    
-    fn intervals() -> Self::IntervalSet;
+const T: Interval = Interval::MAJOR_SECOND;
+const S: Interval = Interval::MINOR_SECOND;
+
+#[repr(u8)]
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Ord, PartialOrd)]
+pub enum DiatonicMode {
+    Ionian = 1,
+    Dorian,
+    Phrygian,
+    Lydian,
+    Mixolydian,
+    Aeolian,
+    Locrian,
 }
 
-pub struct ChromaticScale;
+impl DiatonicMode {
+    pub const MAJOR: Self = Self::Ionian;
+    pub const NATURAL_MINOR: Self = Self::Aeolian;
+    
+    const INTERVALS: [Interval; 7] = [T, T, S, T, T, T, S];
 
-impl Scale for ChromaticScale {
-    type IntervalSet = [Interval; 12];
-
-    fn intervals() -> Self::IntervalSet {
-        let p1 = Interval::PERFECT_UNISON;
+    // TODO: do we like this name?
+    pub fn build_from<T: Add<Interval, Output = T> + Clone>(&self, root: T) -> [T; 7] {
+        let mode = self.number() as usize;
         
-        let a1 = Interval::AUGMENTED_UNISON;
-        let m2 = Interval::MINOR_SECOND;
-        
-        let intervals = [p1, a1, m2, a1, m2, m2, a1, m2, a1, m2, a1, m2];
-        
-        let mut acc = p1;
+        let mut curr = root;
         
         array::from_fn(|i| {
-            acc = acc.add(intervals[i]);
+            let ret = curr.clone();
             
-            acc
+            curr = curr.clone() + Self::INTERVALS[(i + mode - 1) % Self::INTERVALS.len()];
+            
+            ret
         })
+    }
+    
+    pub fn intervals(&self) -> [Interval; 7] {
+        self.build_from(Interval::PERFECT_UNISON)
+    }
+    
+    pub fn number(&self) -> u8 {
+        *self as _
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::pitch::Pitch;
+    use crate::scales::DiatonicMode;
+
+    #[test]
+    fn intervals() {
+        let ivls = DiatonicMode::Locrian.intervals();
+        
+        assert_eq!(ivls, DiatonicMode::Locrian.build_from(Pitch::A).map(|p| Pitch::A.distance_to(&p)));
+        
+        assert_eq!(DiatonicMode::Locrian.build_from(Pitch::A), ivls.map(|i| Pitch::A + i))
     }
 }
