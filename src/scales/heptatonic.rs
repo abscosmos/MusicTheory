68c5832dbbcc9500a2879de2d194396864e38bd1
abscosmos <joshua@dinoslice.com>
2025-03-18 23:41:15 -0500

    Implement hungarian major scale modes & romanian major scale modes

--- src/scales/heptatonic.rs
+++ src/scales/heptatonic.rs
@@ -78,3 +78,35 @@ define_scale!(
     size = 7,
     intervals = [S, A2, T, T, T, S, S],
 );
+
+define_scale!(
+    name = HungarianMajorMode,
+    size = 7,
+    intervals = [A2, S, T, S, T, S, T],
+    mode_aliases = [
+        MAJOR => I,
+        ALTERED_DIMINISHED_DOUBLE_FLAT6 => II,
+        HARMONIC_MINOR_FLAT5 => III,
+        LOCRIAN_NATURAL2 => III,
+        LOCRIAN_NATURAL7 => III,
+        ALTERED_DOMINANT_NATURAL6 => IV,
+        MELODIC_MINOR_SHARP5 => V,
+        UKRAINIAN_DORIAN_FLAT2 => VI,
+        LYDIAN_AUGMENTED_SHARP3 => VII,
+    ],
+);
+
+define_scale!(
+    name = RomanianMajorScale,
+    size = 7,
+    intervals = [S, A2, T, S, T, S, T],
+    mode_aliases = [
+        MAJOR => I,
+        SUPER_LYDIAN_AUGMENTED_NATURAL6 => II,
+        LOCRIAN_NATURAL2_DOUBLE_FLAT7 => III,
+        BLUES_PHRYGIAN_FLAT4 => IV,
+        JAZZ_MINOR_FLAT5 => V,
+        SUPERPHRYGIAN_NATURAL6 => VI,
+        LYDIAN_AUGMENTED_FLAT3 => VII,
+    ],
+);
