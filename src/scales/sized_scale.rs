18c6f44c07b333ae4393bddf7ad1d23f15dce272
abscosmos <joshua@dinoslice.com>
2025-03-30 17:30:37 -0500

    Remove concrete implementations of SizedScale

--- src/scales/sized_scale.rs
+++ src/scales/sized_scale.rs
@@ -1,59 +1,8 @@
 use std::ops::Add;
 use crate::interval::Interval;
-use crate::scales::{build_from, BaseMode};
-
-// const tysize, variable mode, ref; this is 16 bytes :(
-pub struct SizedScaleRef<'a, const N: usize, M: BaseMode<N>> {
-    pub(super) mode: M,
-    pub(super) ivls: &'a [Interval; N],
-}
-
-// const tysize, variable mode, owned
-pub struct SizedScaleOwned<const N: usize, M: BaseMode<N>> {
-    mode: M,
-    ivls: [Interval; N],
-}
-
-impl<const N: usize, M: BaseMode<N>> SizedScaleRef<'_, N, M> {
-    pub fn to_owned(&self) -> SizedScaleOwned<N, M> {
-        SizedScaleOwned {
-            mode: self.mode,
-            ivls: *self.ivls,
-        }
-    }
-}
-
-impl<const N: usize, M: BaseMode<N>> SizedScaleOwned<N, M> {
-    pub fn as_ref_sized_scale(&self) -> SizedScaleRef<N, M> {
-        SizedScaleRef {
-            mode: self.mode,
-            ivls: &self.ivls,
-        }
-    }
-}
 
 pub trait SizedScale<const N: usize> {
     fn relative_intervals(&self) -> [Interval; N];
 
     fn build_from<T: Add<Interval, Output = T> + Clone>(&self, root: T) -> [T; N];
-}
-
-impl<const N: usize, M: BaseMode<N>> SizedScale<N> for SizedScaleRef<'_, N, M> {
-    fn relative_intervals(&self) -> [Interval; N] {
-        *self.ivls
-    }
-
-    fn build_from<T: Add<Interval, Output=T> + Clone>(&self, root: T) -> [T; N] {
-        build_from(self.relative_intervals(), root, self.mode.as_num())
-    }
-}
-
-impl<const N: usize, M: BaseMode<N>> SizedScale<N> for SizedScaleOwned<N, M> {
-    fn relative_intervals(&self) -> [Interval; N] {
-        self.as_ref_sized_scale().relative_intervals()
-    }
-
-    fn build_from<T: Add<Interval, Output=T> + Clone>(&self, root: T) -> [T; N] {
-        self.as_ref_sized_scale().build_from(root)
-    }
 }
