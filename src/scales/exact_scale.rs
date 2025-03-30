715de4d55e8c0a2ffe68e55133acc5e1ae690b8d
abscosmos <joshua@dinoslice.com>
2025-03-30 12:54:38 -0500

    Create ExactScale trait

--- src/scales/exact_scale.rs
+++ src/scales/exact_scale.rs
@@ -3,19 +3,26 @@ use crate::interval::Interval;
 use crate::scales::{DiatonicMode, DiatonicScaleDef, ScaleLike};
 use crate::scales::typed_scale::TypedScale;
 
+pub trait ExactScale<const N: usize, S: ScaleLike<N>>: Default {
+    fn as_typed(&self) -> TypedScale<N, S>;
+}
+
 // const type, const mode
+#[derive(Default)]
 pub struct MajorScale;
 
-impl MajorScale {
-    pub fn base(&self) -> TypedScale<7, DiatonicScaleDef> {
+impl ExactScale<7, DiatonicScaleDef> for MajorScale {
+    fn as_typed(&self) -> TypedScale<7, DiatonicScaleDef> {
         TypedScale::new(DiatonicMode::I)
     }
+}
 
+impl MajorScale {
     pub fn relative_intervals(&self) -> [Interval; 7] {
-        self.base().relative_intervals()
+        self.as_typed().relative_intervals()
     }
 
     pub fn build_from<T: Add<Interval, Output = T> + Clone>(&self, root: T) -> [T; 7] {
-        self.base().build_from(root)
+        self.as_typed().build_from(root)
     }
 }
--- src/scales/typed_scale.rs
+++ src/scales/typed_scale.rs
@@ -1,6 +1,7 @@
 use std::ops::Add;
 use crate::interval::Interval;
 use crate::scales::{build_from, ScaleLike};
+use crate::scales::exact_scale::ExactScale;
 
 // const type, variable mode
 pub struct TypedScale<const N: usize, S: ScaleLike<N>> {
@@ -18,5 +19,10 @@ impl<const N: usize, S: ScaleLike<N>> TypedScale<N, S> {
 
     pub fn build_from<T: Add<Interval, Output = T> + Clone>(&self, root: T) -> [T; N] {
         build_from(self.relative_intervals(), root, &self.mode)
+    }
+    
+    // TODO: does it make sense to keep this method?
+    pub fn make_exact<E: ExactScale<N, S>>() -> E {
+        E::default()
     }
 }
