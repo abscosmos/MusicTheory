15832efc5be9cb523d6fcbda0c64405b08d37b47
abscosmos <joshua@dinoslice.com>
2025-03-30 21:42:15 -0500

    Method to convert SizedScale to DynamicScale

--- src/scales/sized_scale.rs
+++ src/scales/sized_scale.rs
@@ -1,8 +1,13 @@
 use std::ops::Add;
 use crate::interval::Interval;
+use crate::scales::dyn_scale::DynamicScale;
 
 pub trait SizedScale<const N: usize> {
     fn relative_intervals(&self) -> [Interval; N];
 
     fn build_from<T: Add<Interval, Output = T> + Clone>(&self, root: T) -> [T; N];
+    
+    fn to_dyn(&self) -> DynamicScale {
+        DynamicScale::new(self.relative_intervals()).expect("must add up to a P8")
+    }
 }
