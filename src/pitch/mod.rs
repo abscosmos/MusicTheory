f1a434a3e958ab9d0baa93441c7d7af855a77254
abscosmos <joshua@dinoslice.com>
2025-05-14 20:04:23 -0500

    Change Pitch's methods to take in self instead of &self

--- src/pitch/mod.rs
+++ src/pitch/mod.rs
@@ -29,7 +29,7 @@ impl Pitch {
         Self::from_fifths_from_c(pitch)
     }
 
-    pub fn as_fifths_from_c(&self) -> i16 {
+    pub fn as_fifths_from_c(self) -> i16 {
         self.0
     }
 
@@ -37,7 +37,7 @@ impl Pitch {
         Self(fifths)
     }
 
-    pub fn as_pitch_class(&self) -> PitchClass {
+    pub fn as_pitch_class(self) -> PitchClass {
         let fifths_plus_one = self.as_fifths_from_c() + 1;
 
         let semitone_offset = fifths_plus_one.div_euclid(7);
@@ -56,19 +56,19 @@ impl Pitch {
             .expect("i8::rem_euclid(12) must be [0, 12)")
     }
 
-    pub fn chroma(&self) -> u8 {
+    pub fn chroma(self) -> u8 {
         self.as_pitch_class().chroma()
     }
 
     // TODO: is this right?
-    pub fn semitones_to(&self, rhs: Self) -> Semitone {
+    pub fn semitones_to(self, rhs: Self) -> Semitone {
         let lhs = self.as_pitch_class() as u8 as i8;
         let rhs = rhs.as_pitch_class() as u8 as i8;
 
         Semitone((rhs - lhs).rem_euclid(12) as _)
     }
 
-    pub fn letter(&self) -> Letter {
+    pub fn letter(self) -> Letter {
         match (self.as_fifths_from_c() + 1).rem_euclid(7) {
             0 => Letter::F,
             1 => Letter::C,
@@ -81,13 +81,13 @@ impl Pitch {
         }
     }
 
-    pub fn accidental(&self) -> AccidentalSign {
+    pub fn accidental(self) -> AccidentalSign {
         AccidentalSign {
             offset: (self.as_fifths_from_c() + 1).div_euclid(7)
         }
     }
 
-    pub fn semitones_offset_from_c(&self) -> Semitone {
+    pub fn semitones_offset_from_c(self) -> Semitone {
         let fifths_plus_one = self.as_fifths_from_c() + 1;
 
         let n = match fifths_plus_one.rem_euclid(7) {
@@ -104,20 +104,20 @@ impl Pitch {
         Semitone(n as _)
     }
 
-    pub fn simplified(&self) -> Self {
+    pub fn simplified(self) -> Self {
         self.as_pitch_class().bias(self.accidental().offset > 0)
     }
 
-    pub fn enharmonic(&self) -> Self {
+    pub fn enharmonic(self) -> Self {
         self.as_pitch_class().bias(self.accidental().offset < 0)
     }
     
     // TODO: should this function simplify if called with G## & true?
-    pub fn bias(&self, sharp: bool) -> Self {
+    pub fn bias(self, sharp: bool) -> Self {
         self.as_pitch_class().bias(sharp)
     }
 
-    pub fn transpose(&self, interval: Interval) -> Self {
+    pub fn transpose(self, interval: Interval) -> Self {
         use IntervalQuality as Q;
         
         let start = match interval.number().as_simple().number().abs() {
@@ -149,23 +149,23 @@ impl Pitch {
         self.transpose_fifths(dir_offset)
     }
     
-    pub fn distance_to(&self, rhs: &Self) -> Interval {
-        Interval::between_pitches(*self, *rhs)
+    pub fn distance_to(self, rhs: Self) -> Interval {
+        Interval::between_pitches(self, rhs)
     }
 
-    pub fn with_accidental(&self, accidental: AccidentalSign) -> Self {
+    pub fn with_accidental(self, accidental: AccidentalSign) -> Self {
         let letter = self.letter();
 
         Self::from_letter_and_accidental(letter, accidental)
     }
 
-    pub fn with_letter(&self, letter: Letter) -> Self {
+    pub fn with_letter(self, letter: Letter) -> Self {
         let accidental = self.accidental();
 
         Self::from_letter_and_accidental(letter, accidental)
     }
 
-    pub fn transpose_fifths(&self, fifths: i16) -> Self {
+    pub fn transpose_fifths(self, fifths: i16) -> Self {
         let curr = self.as_fifths_from_c();
 
         Self::from_fifths_from_c(curr + fifths)
