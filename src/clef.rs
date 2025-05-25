9ec7b38f88aa8ff6ea21cd99ec065e6eca9209ef
abscosmos <joshua@dinoslice.com>
2025-05-25 10:58:19 -0500

    Make a wrapping range struct for use with clefs

--- src/clef.rs
+++ src/clef.rs
@@ -1,4 +1,5 @@
 use std::num::NonZeroU8;
+use std::ops::RangeInclusive;
 use crate::letter::Letter;
 use crate::note::Note;
 use crate::pitch::Pitch;
@@ -54,3 +55,29 @@ pub struct PercussionClef;
 // should this be represented as a clef?
 #[derive(Copy, Clone, Debug)]
 pub struct TablatureClef;
+
+struct WrappingRange<T: Ord>(RangeInclusive<T>);
+
+impl<T: Ord> WrappingRange<T> {
+    pub fn new(domain: RangeInclusive<T>) -> Self {
+        match domain.into_inner() {
+            (start, end) if start <= end => Self(start..=end),
+            (start, end) => Self(end..=start),
+        }
+    }
+    
+    pub fn contains(&self, range: RangeInclusive<T>, val: &T) -> bool {
+        let start = range.start();
+        let end = range.end();
+        
+        assert!(self.0.contains(val));
+        assert!(self.0.contains(start));
+        assert!(self.0.contains(end));
+        
+        if start <= end {
+            range.contains(val)
+        } else {
+            val >= start || val <= end
+        }
+    }
+}
