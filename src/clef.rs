8c217f5793459489ae0be4a85836416e888b794e
abscosmos <joshua@dinoslice.com>
2025-05-25 09:56:16 -0500

    Create clef constants

--- src/clef.rs
+++ src/clef.rs
@@ -14,6 +14,19 @@ pub struct PitchClef {
 }
 
 impl PitchClef {
+    pub const TREBLE: Self = Self::new(Letter::G, 4, 2).expect("should be valid clef");
+    pub const FRENCH_VIOLIN: Self = Self::new(Letter::G, 4, 1).expect("should be valid clef");
+    pub const BASS: Self = Self::new(Letter::F, 3, 4).expect("should be valid clef");
+    pub const BASS_8VA: Self = Self::new(Letter::F, 4, 4).expect("should be valid clef");
+    pub const BASS_8VB: Self = Self::new(Letter::F, 2, 4).expect("should be valid clef");
+    pub const SUB_BASS: Self = Self::new(Letter::F, 3, 5).expect("should be valid clef");
+    pub const F_BARITONE: Self = Self::new(Letter::F, 3, 3).expect("should be valid clef");
+    pub const SOPRANO: Self = Self::movable_c(1).expect("should be valid clef");
+    pub const MEZZO_SOPRANO: Self = Self::movable_c(2).expect("should be valid clef");
+    pub const ALTO: Self = Self::movable_c(3).expect("should be valid clef");
+    pub const TENOR: Self = Self::movable_c(4).expect("should be valid clef");
+    pub const C_BARITONE: Self = Self::movable_c(5).expect("should be valid clef");
+
     pub const fn new(letter: Letter, octave: i16, staff_line: u8) -> Option<Self> {
         // {range}.contains() isn't const
         let valid_line = 1 <= staff_line && staff_line <= 5;
@@ -25,5 +38,9 @@ impl PitchClef {
         } else {
             None
         }
+    }
+
+    const fn movable_c(staff_line: u8) -> Option<Self> {
+        Self::new(Letter::C, 4, staff_line)
     }
 }
