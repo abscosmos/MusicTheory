15d332fcd4a485883f471364e637a5e4affe3238
abscosmos <joshua@dinoslice.com>
2025-05-31 22:32:03 -0500

    Replace letter & octave in internals of clef with `anchor`

--- src/clef.rs
+++ src/clef.rs
@@ -8,45 +8,47 @@ use crate::util::WrappingRange;
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct PitchClef {
     // assuming there are only G, C, and F clefs
-    letter: Letter,
-    // instead of storing the note that the middle line passes through, 
-    // only store the octave, since the pitch is just the letter.
-    octave: i16,
+    // TODO: better name?
+    anchor: OctaveLetter,
     // are staffs always placed on the line?
     staff_line: NonZeroU8,
 }
 
 impl PitchClef {
-    pub const TREBLE: Self = Self::new(Letter::G, 4, 2).expect("should be valid clef");
-    pub const TREBLE_8VA: Self = Self::new(Letter::G, 5, 2).expect("should be valid clef");
-    pub const TREBLE_8VB: Self = Self::new(Letter::G, 3, 2).expect("should be valid clef");
-    pub const FRENCH_VIOLIN: Self = Self::new(Letter::G, 4, 1).expect("should be valid clef");
-    pub const BASS: Self = Self::new(Letter::F, 3, 4).expect("should be valid clef");
-    pub const BASS_8VA: Self = Self::new(Letter::F, 4, 4).expect("should be valid clef");
-    pub const BASS_8VB: Self = Self::new(Letter::F, 2, 4).expect("should be valid clef");
-    pub const SUB_BASS: Self = Self::new(Letter::F, 3, 5).expect("should be valid clef");
-    pub const F_BARITONE: Self = Self::new(Letter::F, 3, 3).expect("should be valid clef");
+    pub const TREBLE: Self = Self::new(OctaveLetter::new(Letter::G, 4), 2).expect("should be valid clef");
+    pub const TREBLE_8VA: Self = Self::new(OctaveLetter::new(Letter::G, 5), 2).expect("should be valid clef");
+    pub const TREBLE_8VB: Self = Self::new(OctaveLetter::new(Letter::G, 3), 2).expect("should be valid clef");
+    pub const FRENCH_VIOLIN: Self = Self::new(OctaveLetter::new(Letter::G, 4), 1).expect("should be valid clef");
+    pub const BASS: Self = Self::new(OctaveLetter::new(Letter::F, 3), 4).expect("should be valid clef");
+    pub const BASS_8VA: Self = Self::new(OctaveLetter::new(Letter::F, 4), 4).expect("should be valid clef");
+    pub const BASS_8VB: Self = Self::new(OctaveLetter::new(Letter::F, 2), 4).expect("should be valid clef");
+    pub const SUB_BASS: Self = Self::new(OctaveLetter::new(Letter::F, 3), 5).expect("should be valid clef");
+    pub const F_BARITONE: Self = Self::new(OctaveLetter::new(Letter::F, 3), 3).expect("should be valid clef");
     pub const SOPRANO: Self = Self::movable_c(1).expect("should be valid clef");
     pub const MEZZO_SOPRANO: Self = Self::movable_c(2).expect("should be valid clef");
     pub const ALTO: Self = Self::movable_c(3).expect("should be valid clef");
     pub const TENOR: Self = Self::movable_c(4).expect("should be valid clef");
     pub const C_BARITONE: Self = Self::movable_c(5).expect("should be valid clef");
 
-    pub const fn new(letter: Letter, octave: i16, staff_line: u8) -> Option<Self> {
+    pub const fn new(anchor: OctaveLetter, staff_line: u8) -> Option<Self> {
         // {range}.contains() isn't const
         let valid_line = 1 <= staff_line && staff_line <= 5;
         
-        if matches!(letter, Letter::C | Letter::F | Letter::G) && valid_line {
+        if matches!(anchor.letter, Letter::C | Letter::F | Letter::G) && valid_line {
             let staff_line = NonZeroU8::new(staff_line).expect("should be nonzero, already checked.");
             
-            Some(Self{ letter, octave, staff_line })
+            Some(Self{ anchor, staff_line })
         } else {
             None
         }
     }
 
     const fn movable_c(staff_line: u8) -> Option<Self> {
-        Self::new(Letter::C, 4, staff_line)
+        Self::new(OctaveLetter::new(Letter::C, 4), staff_line)
+    }
+    
+    pub fn anchor(self) -> OctaveLetter {
+        self.anchor
     }
     
     // FIXME: this should really return a Letter with an octave
@@ -68,7 +70,7 @@ impl PitchClef {
     fn line(self, line: i8) -> OctaveLetter {
         let letter_delta =  2 * (line - self.staff_line.get() as i8);
 
-        let res_letter = (self.letter.step() as i8 + letter_delta).rem_euclid(7) as _;
+        let res_letter = (self.anchor().letter.step() as i8 + letter_delta).rem_euclid(7) as _;
 
         let res_letter = Letter::from_step(res_letter).expect("% 7 is in range [0,6]");
 
@@ -77,9 +79,9 @@ impl PitchClef {
         let letter_range = WrappingRange::new(Letter::C..=Letter::B);
 
         let range = if letter_delta > 0 {
-            self.letter..=res_letter
+            self.anchor().letter..=res_letter
         } else {
-            res_letter..=self.letter
+            res_letter..=self.anchor().letter
         };
         
         let oct_2 = if *range.start() != Letter::C && letter_range.contains(range, &Letter::C) {
@@ -88,7 +90,7 @@ impl PitchClef {
             0
         };
 
-        OctaveLetter::new(res_letter, self.octave + (oct_1 + oct_2) as i16)
+        OctaveLetter::new(res_letter, self.anchor().octave + (oct_1 + oct_2) as i16)
     }
     
     pub fn get_position(note: Note) -> StaffPosition {
