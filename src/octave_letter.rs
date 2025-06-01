9693dde288f12654934c31d794dc8703c1df3099
abscosmos <joshua@dinoslice.com>
2025-05-31 22:15:19 -0500

    Implement OctaveLetter::with_offset

--- src/octave_letter.rs
+++ src/octave_letter.rs
@@ -17,6 +17,16 @@ impl OctaveLetter {
     pub fn offset_to(self, rhs: Self) -> i16 {
         (rhs.octave - self.octave) * 7 + rhs.letter.step() as i16 - self.letter.step() as i16
     }
+    
+    pub fn with_offset(self, offset: i16) -> Self {
+        let sum = self.letter.step() as i16 + offset;
+        
+        let octave = self.octave + sum.div_euclid(7);
+        
+        let letter = Letter::from_step(sum.rem_euclid(7) as _).expect("must be in range [0,7)");
+        
+        Self::new(letter, octave)
+    }
 }
 
 impl fmt::Display for OctaveLetter {
@@ -49,10 +59,21 @@ mod tests {
     use crate::octave_letter::OctaveLetter;
 
     #[test]
-    fn test_offset() {
+    fn test_offset_to() {
         let c4 = OctaveLetter::new(Letter::C, 4);
         
         assert_eq!(c4.offset_to(OctaveLetter::new(Letter::F, 5)), 10);
         assert_eq!(OctaveLetter::new(Letter::B, 3).offset_to(c4), 1);
+    }
+    
+    #[test]
+    fn test_with_offset() {
+        let e3 = OctaveLetter::new(Letter::E, 3);
+        
+        for offset in -100..=100 {
+            let end = e3.with_offset(offset);
+            
+            assert_eq!(e3.offset_to(end), offset);
+        }
     }
 }
