6e8ac5eaaf4fae659b1255ae785cd4ca4a714201
abscosmos <joshua@dinoslice.com>
2025-06-03 15:52:07 -0500

    Alias Duration as Offset for use in containers

--- src/containers.rs
+++ src/containers.rs
@@ -3,9 +3,12 @@ use crate::duration::Duration;
 use crate::key::Key;
 use crate::note::Note;
 
+// represents duration as offset from container start
+type Offset = Duration;
+
 #[derive(Default, Debug, Clone)]
 pub struct Freeform {
-    elements: Vec<(Duration, ContainerElement)>,
+    elements: Vec<(Offset, ContainerElement)>,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
