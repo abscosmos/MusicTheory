d70aaeac4dd1c3d863301d7a071bf4bb11c6e8c9
abscosmos <joshua@dinoslice.com>
2025-06-03 15:13:47 -0500

    Crude implementation of note container element
    Support for some attributes that apply only to displaying, such as if the accidental should be a courtesy accidental or implied.

--- src/containers.rs
+++ src/containers.rs
@@ -1,7 +1,22 @@
+use crate::note::Note;
+
 pub struct Freeform {
     elements: Vec<ContainerElement>,
 }
 
 pub enum ContainerElement {
-    
+    Note {
+        note: Note,
+        duration: (),
+        accidental: AccidentalDisplay,
+    },
 }
+
+#[derive(Copy, Clone, Debug, Default, Hash, Eq, PartialEq)]
+pub enum AccidentalDisplay {
+    #[default]
+    Normal,
+    Courtesy,
+    Implied,
+}
+
