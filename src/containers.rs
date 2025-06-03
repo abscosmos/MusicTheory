867f66d4a8431ae527e1e4252474574e426aa9e2
abscosmos <joshua@dinoslice.com>
2025-06-03 15:40:00 -0500

    Move duration out of enum

--- src/containers.rs
+++ src/containers.rs
@@ -2,31 +2,19 @@ use crate::duration::Duration;
 use crate::note::Note;
 
 pub struct Freeform {
-    elements: Vec<ContainerElement>,
+    elements: Vec<(Duration, ContainerElement)>,
 }
 
 pub enum ContainerElement {
     Note {
-        // offset as duration from container start
-        offset: Duration,
         note: Note,
         duration: Duration,
         accidental: AccidentalDisplay,
     },
     Rest {
-        offset: Duration,
         duration: Duration,
         implicit: bool,
     },
-}
-
-impl ContainerElement {
-    pub fn offset(&self) -> Duration {
-        match self {
-            ContainerElement::Note { offset, .. } => *offset,
-            ContainerElement::Rest { offset, .. } => *offset,
-        }
-    }
 }
 
 #[derive(Copy, Clone, Debug, Default, Hash, Eq, PartialEq)]
