066ab58a45dd4081dbf5d45acb8199180eeceae6
abscosmos <joshua@dinoslice.com>
2025-11-15 08:13:26 -0600

    Create matrix from numbers (chromas)
    Also, create error for this method since it has two failure paths

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,6 @@
 use crate::pcset::PitchClassSet;
 use crate::pitch_class::PitchClass;
+use crate::prelude::Semitone;
 
 pub struct TwelveToneMatrix([PitchClass; 12]);
 
@@ -9,4 +10,22 @@ impl TwelveToneMatrix {
 
         (pc_set.len() == 12).then_some(Self(prime_0))
     }
+
+    pub fn from_chromas(prime_0: [u8; 12]) -> Result<Self, TwelveToneMatrixFromNumsError> {
+        if prime_0.iter().any(|&n| n >= 12) {
+            return Err(TwelveToneMatrixFromNumsError::InvalidNums);
+        }
+
+        let prime_0 = prime_0.map(PitchClass::from_repr).map(Option::unwrap);
+
+        Self::new(prime_0).ok_or(TwelveToneMatrixFromNumsError::MissingPitches)
+    }
+}
+
+#[derive(thiserror::Error, Debug, Eq, PartialEq)]
+pub enum TwelveToneMatrixFromNumsError {
+    #[error("One or more numbers wasn't a valid pitch class chroma [0,11]")]
+    InvalidNums,
+    #[error("Didn't have all 12 chromas [0,11]")]
+    MissingPitches,
 }
