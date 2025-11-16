3899f09e42346674d1cd0c74b9b550ae185322c1
abscosmos <joshua@dinoslice.com>
2025-11-15 19:31:56 -0600

    Refactor TwelveToneMatrix to use and return TwelveToneRow

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,44 +1,30 @@
-use std::{array, fmt};
+use std::array;
+use std::ops::Deref;
 use crate::pcset::PitchClassSet;
 use crate::pitch_class::PitchClass;
 use crate::prelude::Semitone;
 
-#[derive(Clone, Eq, PartialEq)]
-pub struct TwelveToneMatrix([PitchClass; 12]);
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct TwelveToneMatrix {
+    pub prime_0: TwelveToneRow,
+}
 
 impl TwelveToneMatrix {
-    pub fn new(prime_0: [PitchClass; 12]) -> Option<Self> {
-        let pc_set = PitchClassSet::from_iter(prime_0);
-
-        (pc_set.len() == 12).then_some(Self(prime_0))
-    }
-
-    pub fn from_chromas(prime_0: [u8; 12]) -> Result<Self, TwelveToneMatrixFromNumsError> {
-        if prime_0.iter().any(|&n| n >= 12) {
-            return Err(TwelveToneMatrixFromNumsError::InvalidNums);
-        }
-
-        let prime_0 = prime_0.map(PitchClass::from_repr).map(Option::unwrap);
-
-        Self::new(prime_0).ok_or(TwelveToneMatrixFromNumsError::MissingPitches)
-    }
-
-    pub fn prime(&self, n: u8) -> Option<[PitchClass; 12]> {
+    pub fn prime(&self, n: u8) -> Option<TwelveToneRow> {
         if n >= 12 {
             return None;
         }
 
-        Some(self.0.map(|pc| pc + Semitone(n as _)))
+        let prime_n = self.prime_0.map(|pc| pc + Semitone(n as _));
+
+        Some(TwelveToneRow(prime_n))
     }
 
-    pub fn retrograde(&self, n: u8) -> Option<[PitchClass; 12]> {
-        self.prime(n).map(|mut p| {
-            p.reverse();
-            p
-        })
+    pub fn retrograde(&self, n: u8) -> Option<TwelveToneRow> {
+        self.prime(n).map(|r| r.reverse())
     }
 
-    pub fn inversion(&self, n: u8) -> Option<[PitchClass; 12]> {
+    pub fn inversion(&self, n: u8) -> Option<TwelveToneRow> {
         let prime = self.prime(n)?;
 
         let first = prime[0];
@@ -47,14 +33,11 @@ impl TwelveToneMatrix {
            first - first.semitones_to(pc)
         });
 
-        Some(inversion)
+        Some(TwelveToneRow(inversion))
     }
 
-    pub fn retrograde_inversion(&self, n: u8) -> Option<[PitchClass; 12]> {
-        self.inversion(n).map(|mut p| {
-            p.reverse();
-            p
-        })
+    pub fn retrograde_inversion(&self, n: u8) -> Option<TwelveToneRow> {
+        self.inversion(n).map(|r| r.reverse())
     }
 
     pub fn order_primes(&self) -> [u8; 12] {
@@ -62,37 +45,39 @@ impl TwelveToneMatrix {
     }
 
     pub fn order_inversions(&self) -> [u8; 12] {
-        let first = self.0[0];
+        let first = self.prime_0[0];
 
-        self.0.map(|pc| first.semitones_to(pc).0 as _)
+        self.prime_0.map(|pc| first.semitones_to(pc).0 as _)
     }
 
     pub fn prime_intervals(&self) -> [u8; 12] {
-        Self::intervals_between(self.0)
+        self.prime_0.intervals()
     }
 
     pub fn retrograde_intervals(&self) -> [u8; 12] {
-        Self::intervals_between(self.retrograde(0).expect("r0 must be in range"))
+        self.retrograde(0)
+            .expect("r0 must be in range")
+            .intervals()
     }
 
     pub fn inversion_intervals(&self) -> [u8; 12] {
-        Self::intervals_between(self.inversion(0).expect("r0 must be in range"))
+        self.inversion(0)
+            .expect("i0 must be in range")
+            .intervals()
     }
 
     pub fn retrograde_inversion_intervals(&self) -> [u8; 12] {
-        Self::intervals_between(self.inversion(0).expect("r0 must be in range"))
+        self.retrograde_inversion(0)
+            .expect("ri0 must be in range")
+            .intervals()
     }
 
     pub fn has_all_intervals(&self) -> bool {
-        let ivls = self.prime_intervals();
-
-        (1..12).all(|i| ivls.contains(&i))
+        self.prime_0.has_all_intervals()
     }
 
-    pub fn rotate(&self, offset: u8) -> Self {
-        let mut new = self.clone();
-        new.0.rotate_left(offset as usize % 12);
-        new
+    pub fn rotate(&self, offset: u8) -> Option<Self> {
+        Some(Self { prime_0: self.prime_0.rotate(offset)? })
     }
 
     pub fn table_string(&self) -> String {
@@ -126,26 +111,84 @@ impl TwelveToneMatrix {
 
         s
     }
-
-    fn intervals_between(row: [PitchClass; 12]) -> [u8; 12] {
-        array::from_fn(|i|
-            row[i].semitones_to(row[(i+1) % 12]).0 as _
-        )
-    }
 }
 
-impl fmt::Debug for TwelveToneMatrix {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct(stringify!(TwelveToneMatrix))
-            .field("P-0", &self.0)
-            .finish()
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct TwelveToneRow(pub(crate) [PitchClass; 12]);
+
+impl TwelveToneRow {
+    pub fn new(prime_0: [PitchClass; 12]) -> Option<Self> {
+        let pc_set = PitchClassSet::from_iter(prime_0);
+
+        (pc_set.len() == 12).then_some(Self(prime_0))
+    }
+
+    pub fn from_chromas(prime_0: [u8; 12]) -> Result<Self, TwelveToneRowFromChromasError> {
+        if prime_0.iter().any(|&n| n >= 12) {
+            return Err(TwelveToneRowFromChromasError::InvalidNums);
+        }
+
+        let prime_0 = prime_0.map(|repr|
+            PitchClass::from_repr(repr).expect("just checked, so must be in range")
+        );
+
+        Self::new(prime_0).ok_or(TwelveToneRowFromChromasError::MissingPitches)
+    }
+
+    pub fn reverse(&self) -> Self {
+        let mut new = self.clone();
+        new.0.reverse();
+        new
+    }
+
+    pub fn get(&self) -> &[PitchClass; 12] {
+        &self.0
+    }
+
+    pub fn intervals(&self) -> [u8; 12] {
+        array::from_fn(|i|
+            self.0[i].semitones_to(self.0[(i+1) % 12]).0 as _
+        )
+    }
+
+    pub fn has_all_intervals(&self) -> bool {
+        let ivls = self.intervals();
+
+        (1..12).all(|i| ivls.contains(&i))
+    }
+
+    pub fn rotate(&self, offset: u8) -> Option<Self> {
+        if offset > 12 {
+            None
+        } else {
+            let mut new = self.clone();
+            new.0.rotate_left(offset as usize % 12);
+            Some(new)
+        }
     }
 }
 
 #[derive(thiserror::Error, Debug, Eq, PartialEq)]
-pub enum TwelveToneMatrixFromNumsError {
+pub enum TwelveToneRowFromChromasError {
     #[error("One or more numbers wasn't a valid pitch class chroma [0,11]")]
     InvalidNums,
     #[error("Didn't have all 12 chromas [0,11]")]
     MissingPitches,
+}
+
+impl IntoIterator for TwelveToneRow {
+    type Item = PitchClass;
+    type IntoIter = <[PitchClass; 12] as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl Deref for TwelveToneRow {
+    type Target = [PitchClass; 12];
+
+    fn deref(&self) -> &Self::Target {
+        self.get()
+    }
 }
