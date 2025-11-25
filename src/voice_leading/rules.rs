f493a28d9ead3006d5fc4e502f6754d01ed8e0f3
abscosmos <joshua@dinoslice.com>
2025-11-25 07:43:21 -0600

    Create rules for checking for illegal parallel motion

--- src/voice_leading/rules.rs
+++ src/voice_leading/rules.rs
@@ -23,3 +23,39 @@ pub fn check_spacing(v: Voicing) -> Result<(), (Voice, Voice)> {
 
     Ok(())
 }
+
+fn check_parallel_interval(first: Voicing, second: Voicing, interval: Interval) -> Result<(), (Voice, Voice)> {
+    const NUM_VOICES: usize = 4;
+
+    // make indexing easier
+    let (first, second) = (*first, *second);
+
+    for i in 0..NUM_VOICES {
+        for j in (i + 1)..NUM_VOICES {
+            let v1_first = first[i];
+            let v2_first = first[j];
+            let v1_second = second[i];
+            let v2_second = second[j];
+
+            if v1_first.semitones_to(v2_first) == interval.semitones()
+                && v1_second.semitones_to(v2_second) == interval.semitones()
+            {
+                return Err((
+                    Voice::from_repr(i as u8).expect("valid voice"),
+                    Voice::from_repr(j as u8).expect("valid voice")
+                ));
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub fn check_parallel_fifths(first: Voicing, second: Voicing) -> Result<(), (Voice, Voice)> {
+    check_parallel_interval(first, second, Interval::PERFECT_FIFTH)
+}
+
+pub fn check_parallel_octaves(first: Voicing, second: Voicing) -> Result<(), (Voice, Voice)> {
+    check_parallel_interval(first, second, Interval::PERFECT_OCTAVE)
+}
+
