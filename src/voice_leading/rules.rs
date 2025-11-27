9e503ee8876426ae0c627a6919f8c930f6dc4535
abscosmos <joshua@dinoslice.com>
2025-11-27 08:13:19 -0600

    Scores how many tones kept common

--- src/voice_leading/rules.rs
+++ src/voice_leading/rules.rs
@@ -408,3 +408,38 @@ fn score_melodic_intervals(first: Voicing, second: Voicing) -> u16 {
     penalty
 }
 
+fn score_common_tones(first: Voicing, second: Voicing, first_chord: RomanChord, second_chord: RomanChord, key: Key) -> u16 {
+    assert!(
+        completely_voiced(first, first_chord, key),
+        "first chord must be completely voiced for common tone scoring",
+    );
+
+    assert!(
+        completely_voiced(second, second_chord, key),
+        "second chord must be completely voiced for common tone scoring",
+    );
+
+    let common_pcs = first_chord.pitch_class_set(key) & second_chord.pitch_class_set(key);
+
+    if common_pcs.is_empty() {
+        return 0;
+    }
+
+    let mut penalty = 0;
+
+    for voice in Voice::iter() {
+        let first_note = first[voice];
+        let second_note = second[voice];
+
+        let first_pc = first_note.as_pitch_class();
+
+        if common_pcs.is_set(first_pc) {
+            if first_note != second_note {
+                penalty += 1;
+            }
+        }
+    }
+
+    penalty
+}
+
