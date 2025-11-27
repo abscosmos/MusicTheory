faaf9e6bfb58268a35c404f2889f69bc953f629f
abscosmos <joshua@dinoslice.com>
2025-11-26 19:58:20 -0600

    Ensure leading tone resolves appropriately

--- src/voice_leading/rules.rs
+++ src/voice_leading/rules.rs
@@ -121,3 +121,58 @@ pub fn check_parallel_octaves(first: Voicing, second: Voicing) -> Result<(), (Vo
     check_parallel_interval(first, second, Interval::PERFECT_OCTAVE)
 }
 
+pub fn check_leading_tone_resolution(
+    first: Voicing,
+    second: Voicing,
+    second_chord: RomanChord,
+    key: Key,
+) -> Result<(), Voice> {
+    // for sanity, check the second chord is accurately voiced
+    assert!(
+        completely_voiced(second, second_chord, key),
+        "second chord must be completely voiced for leading tone resolution check",
+    );
+
+    if second_chord.degree != ScaleDegree::I {
+        return Ok(());
+    }
+
+    let leading_tone = {
+        let mut vii = key.scale().build_default()[6];
+
+        if RomanChord::mode_has_raised_leading_tone(key.mode) {
+            vii = vii.transpose(Interval::AUGMENTED_UNISON);
+        }
+
+        vii
+    };
+
+    if leading_tone.distance_to(key.tonic) != Interval::MINOR_SECOND {
+        // this mode does not have a leading tone, so it's fine by default
+        return Ok(());
+    }
+
+    for voice in Voice::iter() {
+        let first_note = first[voice];
+        let second_note = second[voice];
+
+        if first_note.as_pitch_class() == leading_tone.as_pitch_class() {
+            if second_note.as_pitch_class() != key.tonic.as_pitch_class() {
+                return Err(voice);
+            }
+
+            if first_note.semitones_to(second_note) != Interval::MINOR_SECOND.semitones() {
+                return Err(voice);
+            }
+        }
+
+        if first_note.as_pitch_class() == leading_tone.as_pitch_class()
+            && second_note.as_pitch_class() != key.tonic.as_pitch_class()
+        {
+            return Err(voice);
+        }
+    }
+
+    Ok(())
+}
+
