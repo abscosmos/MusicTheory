3f79aa13560ed0b513775d85bda6394df692455a
abscosmos <joshua@dinoslice.com>
2025-11-27 07:54:48 -0600

    Calculate and score motion between outside voices

--- src/voice_leading/rules.rs
+++ src/voice_leading/rules.rs
@@ -337,3 +337,39 @@ pub fn check_chordal_seventh_resolution(
     Ok(())
 }
 
+enum VoiceMotion {
+    Oblique,
+    Contrary,
+    Similar,
+    Parallel,
+}
+
+fn get_motion_between(voice_1: Voice, voice_2: Voice, first: Voicing, second: Voicing) -> VoiceMotion {
+    let soprano_first = first[voice_1];
+    let soprano_second = second[voice_1];
+    let bass_first = first[voice_2];
+    let bass_second = second[voice_2];
+
+    let soprano_motion = soprano_first.distance_to(soprano_second);
+    let bass_motion = bass_first.distance_to(bass_second);
+
+    if soprano_motion == Interval::PERFECT_UNISON && bass_motion == Interval::PERFECT_UNISON {
+        VoiceMotion::Oblique
+    } else if soprano_motion == bass_motion {
+        VoiceMotion::Parallel
+    } else if soprano_motion.is_ascending() != bass_motion.is_ascending() {
+        VoiceMotion::Contrary
+    } else {
+        VoiceMotion::Similar
+    }
+}
+
+pub fn score_outer_voice_motion(first: Voicing, second: Voicing) -> u16 {
+    match get_motion_between(Voice::Soprano, Voice::Bass, first, second) {
+        VoiceMotion::Oblique => 0,
+        VoiceMotion::Contrary => 2,
+        VoiceMotion::Similar => 5,
+        VoiceMotion::Parallel => 10,
+    }
+}
+
