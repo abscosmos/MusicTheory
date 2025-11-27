50284f6e4494a3ba8986e59aa1c49f22bb4e11bc
abscosmos <joshua@dinoslice.com>
2025-11-26 20:50:55 -0600

    Check for unequal fifths

--- src/voice_leading/rules.rs
+++ src/voice_leading/rules.rs
@@ -190,6 +190,36 @@ pub fn check_parallel_octaves(first: Voicing, second: Voicing) -> Result<(), (Vo
     check_parallel_interval(first, second, Interval::PERFECT_OCTAVE)
 }
 
+pub fn check_unequal_fifths(first: Voicing, second: Voicing) -> Result<(), (Voice, Voice)> {
+    for v1 in Voice::iter() {
+        for v2 in Voice::iter() {
+            if v1 == v2 {
+                continue;
+            }
+
+            let v1_first = first[v1];
+            let v2_first = first[v2];
+            let v1_second = second[v1];
+            let v2_second = second[v2];
+
+            let first_interval = v1_first.distance_to(v2_first).as_simple();
+            let second_interval = v1_second.distance_to(v2_second).as_simple();
+
+            let is_perfect_to_dim = first_interval == Interval::PERFECT_FIFTH
+                && second_interval == Interval::DIMINISHED_FIFTH;
+
+            let is_dim_to_perfect = first_interval == Interval::DIMINISHED_FIFTH
+                && second_interval == Interval::PERFECT_FIFTH;
+
+            if is_perfect_to_dim || is_dim_to_perfect {
+                return Err((v1, v2));
+            }
+        }
+    }
+
+    Ok(())
+}
+
 pub fn check_leading_tone_resolution(
     first: Voicing,
     second: Voicing,
