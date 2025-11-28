7f2b7d057fb8c07e56ec28a2ea8a9e5d56164507
abscosmos <joshua@dinoslice.com>
2025-11-28 07:21:58 -0600

    Use brute force search to find all solutions

--- src/voice_leading/solve.rs
+++ src/voice_leading/solve.rs
@@ -2,9 +2,71 @@ use std::ops::RangeInclusive;
 use crate::key::Key;
 use crate::note::Note;
 use crate::pitch::Pitch;
-use crate::voice_leading::check::score_single;
+use crate::voice_leading::check::{check_voice_leading, score_single};
 use crate::voice_leading::roman_chord::RomanChord;
 use crate::voice_leading::{Voice, Voicing};
+
+pub fn brute_force_search(
+    progression: &[RomanChord],
+    starting_voicing: Option<Voicing>,
+    key: Key,
+) -> Vec<(u16, Vec<Voicing>)> {
+    if progression.len() == 0 {
+        return vec![];
+    }
+
+    let mut all_voicings: Vec<Vec<Voicing>> = progression
+        .iter()
+        .map(|chord| generate_voicings_for_chord(*chord, key))
+        .collect();
+
+    if let Some(start) = starting_voicing {
+        all_voicings[0] = vec![start];
+    }
+
+    let total_permutations: usize = all_voicings
+        .iter()
+        .map(|v| v.len())
+        .product();
+
+    dbg!(total_permutations);
+
+    if total_permutations == 0 || all_voicings.is_empty() {
+        return vec![];
+    }
+
+    let mut results: Vec<Vec<Voicing>> = vec![vec![]];
+
+    for chord_voicings in all_voicings {
+        let mut new_results = Vec::with_capacity(results.len() * chord_voicings.len());
+
+        for seq in &results {
+            for voicing in &chord_voicings {
+                let mut new_seq = seq.clone();
+                new_seq.push(*voicing);
+                new_results.push(new_seq);
+            }
+        }
+
+        results = new_results;
+    }
+
+    assert_eq!(
+        results.len(), total_permutations,
+        "should've generated all, exactly"
+    );
+
+    let mut results = results.into_iter()
+        .filter_map(|v| match check_voice_leading(key, progression, &v) {
+            Ok(score) => Some((score, v)),
+            Err(_) => None,
+        })
+        .collect::<Vec<_>>();
+
+    results.sort_by_key(|(score, _)| *score);
+
+    results
+}
 
 fn generate_voicings_for_chord(chord: RomanChord, key: Key) -> Vec<Voicing> {
     let pitches = chord.pitches(key);
