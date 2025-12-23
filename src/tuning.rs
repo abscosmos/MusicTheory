ca6d76e314dbd88e9b29c6cb0189bb00c2c0f50c
abscosmos <joshua@dinoslice.com>
2025-12-23 14:55:27 -0600

    Create struct for 12TET

--- src/tuning.rs
+++ src/tuning.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Serialize};
 use crate::note::Note;
 
 // TODO: cents type
@@ -6,4 +7,9 @@ pub trait Tuning {
     fn freq_to_note(&self, hz: f32) -> (Note, f32);
 
     fn note_to_freq_hz(&self, note: Note) -> f32;
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct TwelveToneEqualTemperament {
+    a4_hz: f32,
 }
