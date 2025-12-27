9708a61792ad9a79b6802166c2c143d12451a343
abscosmos <joshua@dinoslice.com>
2025-12-27 15:22:37 -0600

    Also bench converting frequency to note
    Results were extremely close:
    original: [2.6567 µs 2.6761 µs 2.7017 µs]
    ratios: [2.6573 µs 2.6637 µs 2.6724 µs]

--- benches/bench_tuning.rs
+++ benches/bench_tuning.rs
@@ -4,7 +4,8 @@ use music_theory::note::Note;
 use music_theory::tuning::{JustIntonation, Tuning, TwelveToneEqualTemperament};
 
 fn twelve_tet_tuning(c: &mut Criterion) {
-    let mut g = c.benchmark_group("12TET");
+    let mut g = c.benchmark_group("12TET, note to freq");
+    g.sample_size(300);
 
     let notes = (u8::MIN..=u8::MAX).map(Note::from_midi).collect::<Vec<_>>();
 
@@ -20,6 +21,26 @@ fn twelve_tet_tuning(c: &mut Criterion) {
     g.bench_function("ratios", |b| b.iter(|| {
         for note in &notes {
             let _hz = black_box(tuning_ratios.note_to_freq_hz(black_box(*note)));
+        }
+    }));
+
+    drop(g);
+
+    let freq_original = notes.iter().map(|&n| tuning_eq_temp.note_to_freq_hz(n).expect("valid notes")).collect::<Vec<_>>();
+    let freq_ratios = notes.iter().map(|&n| tuning_ratios.note_to_freq_hz(n).expect("valid notes")).collect::<Vec<_>>();
+
+    let mut g = c.benchmark_group("12TET, freq to note");
+    g.sample_size(300);
+
+    g.bench_function("original", |b| b.iter(|| {
+        for freq in &freq_original {
+            let _ret = black_box(tuning_eq_temp.freq_to_note(black_box(*freq)));
+        }
+    }));
+
+    g.bench_function("ratios", |b| b.iter(|| {
+        for freq in &freq_ratios {
+            let _ret = black_box(tuning_eq_temp.freq_to_note(black_box(*freq)));
         }
     }));
 }
