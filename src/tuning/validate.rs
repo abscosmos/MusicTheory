2a25f20ff01df1af7f2fd6f5af898d114a6d175b
abscosmos <joshua@dinoslice.com>
2025-12-31 19:35:55 -0600

    Have tuning::validate method check computable, valid inverses, and cents within threshold
    ValidRangesCentsThreshold's relative threshold doesn't really work

--- src/tuning/validate.rs
+++ src/tuning/validate.rs
@@ -1,23 +1,77 @@
-use std::ops::RangeInclusive;
+use std::ops::{ControlFlow, RangeInclusive};
+use typed_floats::tf32::{self, PositiveFinite};
 use crate::generator::NoteGenerator;
 use crate::note::Note;
 use crate::tuning::Tuning;
 
 #[derive(Clone, Debug, Eq, PartialEq, thiserror::Error)]
-pub enum ComputableRangeError {
+pub enum ValidRangesError {
     #[error("Given start note wasn't computable")]
     StartNotComputable,
     #[error("Provided range did not contain start")]
     InvalidCheckRange,
 }
 
-pub fn computable_range(tuning: &impl Tuning, start: Note, check_range: Option<RangeInclusive<Note>>) -> Result<RangeInclusive<Note>, ComputableRangeError> {
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct ValidRangesCentsThreshold {
+    pub threshold: PositiveFinite,
+    pub relative_threshold: PositiveFinite
+}
+
+impl ValidRangesCentsThreshold {
+    pub const EXACT: Self = {
+        let Ok(zero) = PositiveFinite::new(0.0) else {
+            panic!("unreachable!: 0.0 is in [0, inf)");
+        };
+
+        Self {
+            threshold: zero,
+            relative_threshold: zero,
+        }
+    };
+
+    pub const UNCHECKED: Self = {
+        let Ok(max) = PositiveFinite::new(tf32::MAX.get()) else {
+            panic!("unreachable!: 0.0 is in [0, inf)");
+        };
+
+        Self {
+            threshold: max,
+            relative_threshold: max,
+        }
+    };
+
+    pub fn absolute(threshold: PositiveFinite) -> Self {
+        Self {
+            threshold,
+            .. Self::EXACT
+        }
+    }
+}
+
+impl Default for ValidRangesCentsThreshold {
+    fn default() -> Self {
+        Self {
+            threshold: PositiveFinite::new(1e-9).expect("is in [0, inf)"),
+            relative_threshold: PositiveFinite::new(1e-3).expect("is in [0, inf)"),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ValidRangesResult {
+    pub computable: RangeInclusive<Note>,
+    pub valid_inverses: Option<RangeInclusive<Note>>,
+    pub cents_within_threshold: Option<RangeInclusive<Note>>,
+}
+
+pub fn valid_ranges(tuning: &impl Tuning, start: Note, check_range: Option<RangeInclusive<Note>>, cents_threshold: ValidRangesCentsThreshold) -> Result<ValidRangesResult, ValidRangesError> {
     // no need to check for empty, since the contains test will catch that
     if check_range.as_ref().is_some_and(|range| !range.contains(&start)){
-        return Err(ComputableRangeError::InvalidCheckRange);
+        return Err(ValidRangesError::InvalidCheckRange);
     }
 
-    let range_below = match &check_range {
+    let mut range_below = match &check_range {
         Some(range) => NoteGenerator::range(start, *range.start()),
         None => {
             let mut note_gen = NoteGenerator::reversed(start).take_until_overflow();
@@ -33,29 +87,73 @@ pub fn computable_range(tuning: &impl Tuning, start: Note, check_range: Option<R
         },
     };
 
-    let is_computable = |note: &Note| -> bool {
-        let Some(freq_hz) = tuning.note_to_freq_hz(*note) else {
-            return false;
-        };
-
-        let Some(_) = tuning.freq_to_note(freq_hz) else {
-            return false;
-        };
-
-        true
-    };
-
-    let range_above = match &check_range {
+    let mut range_above = match &check_range {
         Some(range) => NoteGenerator::range_inclusive(start, *range.end()),
         None => NoteGenerator::new(start).take_until_overflow(),
     };
 
-    // the only way this returns none is if the first element (start) isn't computable
-    let above_fail = range_above.take_while(is_computable).last()
-        .ok_or(ComputableRangeError::StartNotComputable)?;
+    #[derive(Default)]
+    struct State {
+        pub last_computable: Option<Note>,
 
-    // fine to unwrap with start, because start is already shown to be valid
-    let below_fail = range_below.take_while(is_computable).last().unwrap_or(start);
+        pub last_valid_inverse: Option<Note>,
+        pub hit_invalid_inverse: bool,
 
-    Ok(below_fail..=above_fail)
+        pub last_valid_cents: Option<Note>,
+        pub hit_invalid_cents: bool,
+    }
+
+    let try_fold = |mut state: State, note: Note| -> ControlFlow<State, State> {
+        // 1. check computable
+        let Some(freq_hz) = tuning.note_to_freq_hz(note) else {
+            return ControlFlow::Break(state);
+        };
+
+        let Some((comp_note, cents)) = tuning.freq_to_note(freq_hz) else {
+            return ControlFlow::Break(state);
+        };
+
+        state.last_computable = Some(note);
+
+        // 2. valid inverse
+        if !state.hit_invalid_inverse && note == comp_note {
+            state.last_valid_inverse = Some(note);
+        } else {
+            state.hit_invalid_inverse = true;
+        }
+
+        // 3. cents in threshold
+        let cents_abs = cents.get().abs();
+
+        if !state.hit_invalid_cents && !state.hit_invalid_inverse
+            && (cents_abs < cents_threshold.threshold || cents_abs / freq_hz.get() < cents_threshold.relative_threshold)
+        {
+            state.last_valid_cents = Some(note);
+        } else {
+            state.hit_invalid_cents = true;
+        }
+
+        ControlFlow::Continue(state)
+    };
+
+    let above = match range_above.try_fold(State::default(), try_fold) {
+        ControlFlow::Continue(s) | ControlFlow::Break(s) => s
+    };
+
+    let below = match range_below.try_fold(State::default(), try_fold) {
+        ControlFlow::Continue(s) | ControlFlow::Break(s) => s
+    };
+
+    // fine to unwrap below with start, because start is already shown to be valid in all cases
+    let res = ValidRangesResult {
+        computable: below.last_computable.unwrap_or(start)..=above.last_computable.ok_or(ValidRangesError::StartNotComputable)?,
+        valid_inverses: above.last_valid_inverse.map(|above|
+            below.last_valid_inverse.unwrap_or(start)..=above
+        ),
+        cents_within_threshold: above.last_valid_cents.map(|above|
+            below.last_valid_cents.unwrap_or(start)..=above
+        ),
+    };
+
+    Ok(res)
 }
