d4fceba73fc513d607349698a7eba3e6b76f4a8c
abscosmos <joshua@dinoslice.com>
2026-01-17 10:19:41 -0600

    Add docs for ScaleDegree

--- src/harmony/scale_degree.rs
+++ src/harmony/scale_degree.rs
@@ -1,5 +1,11 @@
 use crate::scales::numeral::Numeral7 as ScaleDegreeExp;
 
+/// Enum of scale degrees in a heptatonic scale.
+///
+/// Copy of [implementation in experimental scales module][exp].
+/// Intended to be used until a stable version of scales is released.
+///
+/// [exp]: crate::scales::numeral::Numeral7
 #[repr(u8)]
 #[derive(Copy, Clone, Eq, PartialEq, Default, Debug, Ord, PartialOrd, strum_macros::FromRepr)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -15,10 +21,26 @@ pub enum ScaleDegree {
 }
 
 impl ScaleDegree {
+    /// Convert `self` to a `u8`, starting from `1`.
+    ///
+    /// # Examples
+    /// ```rust
+    /// # use music_theory::harmony::ScaleDegree;
+    /// assert_eq!(ScaleDegree::III.as_num(), 3);
+    /// ```
     pub fn as_num(self) -> u8 {
         self as _
     }
 
+    /// Convert from `u8` to `self`, *starting from `1`*.
+    ///
+    /// Returns `None` if not in [1, 7].
+    ///
+    /// # Examples
+    /// ```rust
+    /// # use music_theory::harmony::ScaleDegree;
+    /// assert_eq!(ScaleDegree::from_num(3), Some(ScaleDegree::III));
+    /// ```
     pub fn from_num(num: u8) -> Option<Self> {
         Self::from_repr(num)
     }
