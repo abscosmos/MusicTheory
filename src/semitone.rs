f4d379ab9c9ab839dba5bfca86a29dd4a761ae44
abscosmos <joshua@dinoslice.com>
2026-01-17 11:07:54 -0600

    Implement remaining arithmetic operator overloads for Semitone

--- src/semitone.rs
+++ src/semitone.rs
@@ -1,5 +1,20 @@
-use std::ops::{Add, Neg, Sub};
+use std::ops::{Add, AddAssign, Div, Mul, Neg, Sub, SubAssign};
 
+/// A signed distance in semitones.
+///
+/// This is equivalent to a "half-step", and can be used to represent chromatic distances or
+/// spelling agnostic intervals.
+///
+/// # Examples
+/// ```
+/// # use music_theory::prelude::*;
+/// let fifth = Semitone(7);
+/// let octave = Semitone::OCTAVE;
+///
+/// assert_eq!(fifth + fifth, Semitone(14));
+/// assert_eq!(octave - fifth, Semitone(5));
+/// assert_eq!(Semitone(14).mod_octave(), Semitone(2));
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Default, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Semitone(pub i16);
@@ -25,5 +40,33 @@ impl Neg for Semitone {
 
     fn neg(self) -> Self::Output {
         Self(-self.0)
+    }
+}
+
+impl AddAssign for Semitone {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 += rhs.0;
+    }
+}
+
+impl SubAssign for Semitone {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.0 -= rhs.0;
+    }
+}
+
+impl Mul<i16> for Semitone {
+    type Output = Semitone;
+
+    fn mul(self, rhs: i16) -> Self::Output {
+        Self(self.0 * rhs)
+    }
+}
+
+impl Div<i16> for Semitone {
+    type Output = Semitone;
+
+    fn div(self, rhs: i16) -> Self::Output {
+        Self(self.0 / rhs)
     }
 }
