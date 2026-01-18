5d8299c3c505a01a52edd032c49b51d2715f5e57
abscosmos <joshua@dinoslice.com>
2026-01-18 13:17:08 -0600

    Implement Add/Sub Semitones for pcset

--- src/set/pcset/mod.rs
+++ src/set/pcset/mod.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::ops::{BitAnd, BitXor, Not};
+use std::ops::{Add, BitAnd, BitXor, Not, Sub};
 use strum::IntoEnumIterator;
 use crate::pitch::PitchClass;
 use crate::set::IntervalClassVector;
@@ -81,6 +81,8 @@ impl PitchClassSet {
     }
 
     /// Transpose all pitch classes by the given number of semitones.
+    ///
+    /// This is equivalent to using the [+ operator](Add::add).
     ///
     /// # Examples
     ///
@@ -182,6 +184,72 @@ impl BitXor for PitchClassSet {
 
     fn bitxor(self, rhs: Self) -> Self::Output {
         self.symmetric_difference(rhs)
+    }
+}
+
+impl Add<Semitones> for PitchClassSet {
+    type Output = Self;
+
+    /// Transpose the pitch class set up by the given number of semitones.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use music_theory::prelude::*;
+    /// # use music_theory::set::PitchClassSet;
+    /// // C major triad [C, E, G]
+    /// let c_major = PitchClassSet::from_iter([
+    ///     PitchClass::C,
+    ///     PitchClass::E,
+    ///     PitchClass::G,
+    /// ]);
+    ///
+    /// // Transpose up by 2 semitones to get D major [D, Fs, A]
+    /// let d_major = c_major + Semitones(2);
+    /// assert_eq!(
+    ///     d_major,
+    ///     PitchClassSet::from_iter([
+    ///         PitchClass::D,
+    ///         PitchClass::Fs,
+    ///         PitchClass::A,
+    ///     ]),
+    /// );
+    /// ```
+    fn add(self, rhs: Semitones) -> Self::Output {
+        self.transpose(rhs)
+    }
+}
+
+impl Sub<Semitones> for PitchClassSet {
+    type Output = Self;
+
+    /// Transpose the pitch class set down by the given number of semitones.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use music_theory::prelude::*;
+    /// # use music_theory::set::PitchClassSet;
+    /// // C major triad [C, E, G]
+    /// let c_major = PitchClassSet::from_iter([
+    ///     PitchClass::C,
+    ///     PitchClass::E,
+    ///     PitchClass::G,
+    /// ]);
+    ///
+    /// // Transpose down by 3 semitones to get A major [A, Cs, E]
+    /// let a_major = c_major - Semitones(3);
+    /// assert_eq!(
+    ///     a_major,
+    ///     PitchClassSet::from_iter([
+    ///         PitchClass::A,
+    ///         PitchClass::Cs,
+    ///         PitchClass::E,
+    ///     ])
+    /// );
+    /// ```
+    fn sub(self, rhs: Semitones) -> Self::Output {
+        self + (-rhs)
     }
 }
 
