a4a68464e002516e780dbd448ee43418e093b607
abscosmos <joshua@dinoslice.com>
2026-01-18 19:22:42 -0600

    Add documentation about implementing Enharmonic{Eq, Ord} for types that implement WithoutSpelling

--- src/enharmonic/without_spelling.rs
+++ src/enharmonic/without_spelling.rs
@@ -8,8 +8,65 @@ use crate::enharmonic::{EnharmonicEq, EnharmonicOrd};
 /// [`PitchClass`] discards the specific spelling (C# vs Db) and retains only the
 /// chromatic position.
 ///
-/// Types that implement this trait automatically get [`EnharmonicEq`] and [`EnharmonicOrd`]
-/// implementations through blanket impls.
+/// # Supertraits
+/// This requires [`EnharmonicEq`] and [`EnharmonicOrd`] to be implemented.
+/// Unless [`WithoutSpelling::without_spelling`] is expensive for your type, and you can implement
+/// [`EnharmonicEq`] and [`EnharmonicOrd`] cheaper, you should implement it like this:
+/// ```
+/// use music_theory::enharmonic::{WithoutSpelling, EnharmonicEq, EnharmonicOrd};
+/// use std::cmp::Ordering;
+/// #
+/// # #[derive(Clone)]
+/// # pub struct YourType;
+/// # impl Copy for YourType {}
+/// #
+/// # pub struct YourTypeUnspelled;
+/// #
+/// # impl PartialEq for YourTypeUnspelled {
+/// #     fn eq(&self, other: &Self) -> bool {
+/// #         unimplemented!()
+/// #     }
+/// # }
+/// # impl Eq for YourTypeUnspelled {}
+/// #
+/// # impl PartialOrd for YourTypeUnspelled {
+/// #     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+/// #         Some(self.cmp(other))
+/// #     }
+/// # }
+/// #
+/// # impl Ord for YourTypeUnspelled {
+/// #     fn cmp(&self, other: &Self) -> Ordering {
+/// #         unimplemented!()
+/// #     }
+/// # }
+///
+/// impl WithoutSpelling for YourType {
+///     type Unspelled = YourTypeUnspelled;
+///
+///     fn without_spelling(self) -> Self::Unspelled {
+///         unimplemented!()
+///     }
+/// }
+///
+/// impl EnharmonicEq for YourType {
+///     fn eq_enharmonic(&self, other: &Self) -> bool {
+///         // You may need to clone 'self' and 'other' here,
+///         // if 'YourType' isn't copy. If you can implement
+///         // 'YourType::eq_enharmonic' cheaper than the combined
+///         // cost of 'YourType::clone' and 'YourType::without_spelling',
+///         // then *don't* do this.
+///         self.without_spelling() == other.without_spelling()
+///     }
+/// }
+///
+/// impl EnharmonicOrd for YourType {
+///     fn cmp_enharmonic(&self, other: &Self) -> Ordering {
+///         // See the comment about 'eq_enharmonic' above
+///         self.without_spelling().cmp(&other.without_spelling())
+///     }
+/// }
+/// ```
 ///
 /// # Examples
 ///
