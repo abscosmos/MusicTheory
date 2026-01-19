ab98169ea3c8bdc4baedebcf4c8a77db01d80ba3
abscosmos <joshua@dinoslice.com>
2026-01-18 18:46:06 -0600

    Move CmpEnharmonic wrapper to its own module

---
+++ src/enharmonic/wrapper.rs
@@ -0,0 +1,80 @@
+use std::cmp::Ordering;
+use std::hash::{Hash, Hasher};
+use crate::enharmonic::{EnharmonicEq, EnharmonicOrd, WithoutSpelling};
+
+/// A wrapper that implements standard comparison traits using enharmonic comparison.
+///
+/// This type allows using enharmonic comparison with standard library collections
+/// and algorithms that require [`Ord`], such as [`BTreeMap`], [`BTreeSet`], and
+/// sorting methods.
+///
+/// The wrapper implements [`PartialEq`], [`Eq`], [`PartialOrd`], and [`Ord`] by
+/// delegating to the wrapped type's [`EnharmonicEq`] and [`EnharmonicOrd`] implementations.
+///
+/// # Examples
+///
+/// Using in a sorted collection:
+///
+/// ```
+/// # use music_theory::prelude::*;
+/// use music_theory::enharmonic::CmpEnharmonic;
+/// use std::collections::BTreeSet;
+///
+/// let mut set = BTreeSet::new();
+/// set.insert(CmpEnharmonic(Pitch::C_SHARP));
+/// set.insert(CmpEnharmonic(Pitch::D_FLAT));
+/// set.insert(CmpEnharmonic(Pitch::E));
+///
+/// // C# and Db are enharmonically equivalent, so only one is kept
+/// assert_eq!(set.len(), 2);
+/// ```
+///
+/// Using as a HashMap key:
+///
+/// ```
+/// # use music_theory::prelude::*;
+/// use music_theory::enharmonic::CmpEnharmonic;
+/// use std::collections::HashMap;
+///
+/// let mut map = HashMap::new();
+/// map.insert(CmpEnharmonic(Pitch::C_SHARP), "C# major");
+///
+/// // D♭ is enharmonically equivalent to C#, so it maps to the same value
+/// assert_eq!(map.get(&CmpEnharmonic(Pitch::D_FLAT)), Some(&"C# major"));
+/// ```
+///
+/// [`BTreeMap`]: std::collections::BTreeMap
+/// [`BTreeSet`]: std::collections::BTreeSet
+#[repr(transparent)]
+#[derive(Copy, Clone, Debug, Default)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct CmpEnharmonic<T>(pub T);
+
+impl<T: EnharmonicEq> PartialEq for CmpEnharmonic<T> {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq_enharmonic(&other.0)
+    }
+}
+
+impl<T: EnharmonicEq> Eq for CmpEnharmonic<T> {}
+
+impl<T: EnharmonicOrd + EnharmonicEq> Ord for CmpEnharmonic<T> {
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.cmp_enharmonic(&other.0)
+    }
+}
+
+impl<T: EnharmonicOrd + EnharmonicEq> PartialOrd for CmpEnharmonic<T> {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T: WithoutSpelling<Unspelled: Hash> + Copy> Hash for CmpEnharmonic<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.without_spelling().hash(state);
+    }
+}
