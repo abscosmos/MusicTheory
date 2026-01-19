c1b6cfd318b25e3c326a1b6977b50a167e41bf10
abscosmos <joshua@dinoslice.com>
2026-01-18 18:00:30 -0600

    Add freestanding ordering comparison methods in enharmonic module
    Similar to std::cmp

--- src/enharmonic.rs
+++ src/enharmonic.rs
@@ -267,3 +267,55 @@ pub trait EnharmonicOrd {
         }
     }
 }
+
+/// Compares and returns the maximum of two values enharmonically.
+///
+/// Returns the second argument if the comparison determines them to be enharmonically equal.
+///
+/// # Examples
+///
+/// ```
+/// # use music_theory::prelude::*;
+/// use music_theory::enharmonic;
+///
+/// assert_eq!(enharmonic::max(Pitch::C, Pitch::E), Pitch::E);
+/// ```
+#[inline]
+pub fn max<T: EnharmonicOrd>(v1: T, v2: T) -> T {
+    v1.max_enharmonic(v2)
+}
+
+/// Compares and returns the minimum of two values enharmonically.
+///
+/// Returns the first argument if the comparison determines them to be enharmonically equal.
+///
+/// # Examples
+///
+/// ```
+/// # use music_theory::prelude::*;
+/// use music_theory::enharmonic;
+///
+/// assert_eq!(enharmonic::min(Pitch::C, Pitch::E), Pitch::C);
+/// ```
+#[inline]
+pub fn min<T: EnharmonicOrd>(v1: T, v2: T) -> T {
+    v1.min_enharmonic(v2)
+}
+
+/// Compares and returns the minimum and maximum of two values enharmonically.
+///
+/// Returns `(v1, v2)` if the comparison determines them to be enharmonically equal.
+///
+/// # Examples
+///
+/// ```
+/// # use music_theory::prelude::*;
+/// use music_theory::enharmonic;
+///
+/// assert_eq!(enharmonic::minmax(Pitch::E, Pitch::C), (Pitch::C, Pitch::E));
+/// assert_eq!(enharmonic::minmax(Pitch::C, Pitch::E), (Pitch::C, Pitch::E));
+/// ```
+#[inline]
+pub fn minmax<T: EnharmonicOrd>(v1: T, v2: T) -> (T, T) {
+    if v2.lt_enharmonic(&v1) { (v2, v1) } else { (v1, v2) }
+}
