7a513f0caed361c6810f71ae634a1133764c1d01
abscosmos <joshua@dinoslice.com>
2026-01-22 07:42:38 -0600

    Create rule for range

---
+++ src/voice_leading/rules/range.rs
@@ -0,0 +1,52 @@
+use std::ops::RangeInclusive;
+use strum::IntoEnumIterator;
+use crate::Note;
+use crate::voice_leading::{Voice, Voicing};
+
+pub struct RangeRule {
+    ranges: [RangeInclusive<Note>; 4],
+}
+
+impl RangeRule {
+    pub fn new(
+        soprano: RangeInclusive<Note>,
+        alto: RangeInclusive<Note>,
+        tenor: RangeInclusive<Note>,
+        bass: RangeInclusive<Note>,
+    ) -> Result<Self, (Voice, Voice)> {
+        let ranges = [soprano, alto, tenor, bass];
+        
+        match ranges.windows(2)
+            .position(|ranges_pair| {
+                let [top, bottom] = &ranges_pair else {
+                    unreachable!("windows(2) should have exactly two elements");
+                };
+
+                top.start() < bottom.start() || top.end() < bottom.end()
+            })
+        {
+            None => Ok(Self { ranges }),
+            Some(error) => {
+                let first = Voice::from_repr(error as _).expect("must be in range");
+                let second = Voice::from_repr(error as u8 + 1).expect("must be in range");
+                
+                Err((first, second))
+            }
+        }
+    }
+    
+    #[inline]
+    pub fn range(&self, v: Voice) -> RangeInclusive<Note> {
+        self.ranges[v as usize].clone()
+    }
+    
+    pub fn evaluate(&self, v: Voicing) -> Result<(), Voice> {
+        for voice in Voice::iter() {
+            if !self.range(voice).contains(&v[voice]) {
+                return Err(voice)
+            }
+        }
+        
+        Ok(())
+    }
+}
