246ad7dc70b51d54454ba7f59cc21948281be9ed
abscosmos <joshua@dinoslice.com>
2026-02-11 06:31:52 -0600

    Rename tonic field to root

--- src/chord/pitch.rs
+++ src/chord/pitch.rs
@@ -4,7 +4,7 @@ use crate::{Interval, Pitch};
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct PitchChord {
-    tonic: Pitch,
+    root: Pitch,
     shape: ChordShape,
     bass: Option<Interval>,
 }
