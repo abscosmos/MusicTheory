16665ab57d7e38971b365569fabad75bf4768746
abscosmos <joshua@dinoslice.com>
2026-02-11 06:35:53 -0600

    Add TODO about PitchChord field invariants

--- src/chord/pitch.rs
+++ src/chord/pitch.rs
@@ -4,6 +4,7 @@ use crate::{Interval, Pitch};
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct PitchChord {
+    // TODO: should the root & shape fields be public? What the invariant that must be held?
     root: Pitch,
     shape: ChordShape,
     bass: Option<Interval>,
