6d8f3d2444f60ea7de6618b38df47fced6791ef3
abscosmos <joshua@dinoslice.com>
2026-02-11 19:46:07 -0600

    Add slice query methods, delegate to underlying vector

--- src/chord/note.rs
+++ src/chord/note.rs
@@ -14,4 +14,12 @@ impl NoteChord {
     pub fn from_pitch_chord(chord: &PitchChord, bass_octave: i16) -> Self {
         Self::new(chord.notes(bass_octave))
     }
+
+    pub fn len(&self) -> usize {
+        self.notes.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.notes.is_empty()
+    }
 }
