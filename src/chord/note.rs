7aefc9ada60b4952cc4b04202c3625085b97f6a7
abscosmos <joshua@dinoslice.com>
2026-02-11 19:44:09 -0600

    Create ctors for NoteChord

--- src/chord/note.rs
+++ src/chord/note.rs
@@ -1,5 +1,17 @@
+use crate::chord::pitch::PitchChord;
 use crate::Note;
 
 pub struct NoteChord {
     notes: Vec<Note>,
+}
+
+impl NoteChord {
+    // TODO: the existence of this method allows empty chords; is this valid?
+    pub fn new(notes: impl IntoIterator<Item=Note>) -> Self {
+        Self { notes: notes.into_iter().collect() }
+    }
+
+    pub fn from_pitch_chord(chord: &PitchChord, bass_octave: i16) -> Self {
+        Self::new(chord.notes(bass_octave))
+    }
 }
