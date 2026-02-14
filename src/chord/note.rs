93dd0299ceb13521a77834965c53bdea6e909324
abscosmos <joshua@dinoslice.com>
2026-02-14 11:23:33 -0600

    Start writing method to revoice chord in closed position

--- src/chord/note.rs
+++ src/chord/note.rs
@@ -1,6 +1,8 @@
+use std::cmp::Ordering;
 use crate::chord::pitch::PitchChord;
-use crate::{Interval, Note, Pitch};
+use crate::{Interval, Letter, Note, Pitch};
 use crate::chord::root;
+use crate::chord::letter_set::LetterSet;
 use crate::set::PitchClassSet;
 
 pub struct NoteChord {
@@ -77,6 +79,138 @@ impl NoteChord {
 
     pub fn contains_note(&self, note: Note) -> bool {
         self.notes.contains(&note)
+    }
+
+    pub fn closed_position(&self, separate_steps: bool) -> Self {
+        // TODO: potentially make this a vecdeque
+        let mut notes = self.notes.to_vec();
+
+        let mut closed = Vec::with_capacity(self.len());
+
+        let mut letters_added = LetterSet::EMPTY;
+
+        // TODO: dedup same pitch different octave!!
+
+        // 1. add root(s)
+        let root = {
+            let root_pos = notes.iter()
+                .position(|n| n.pitch == self.root)
+                .expect("root should exist in chord");
+
+            notes.remove(root_pos)
+        };
+
+        closed.push(root);
+        letters_added = letters_added.with_set(root.pitch.letter());
+
+        let roots = notes.extract_if(.., |n|
+            n.pitch.letter() == self.root.letter()
+        );
+
+        for (i, note) in roots.enumerate() {
+            let octave = root.octave + 1;
+            let new_octave = if separate_steps { octave + i as i16 } else { octave };
+
+            closed.push(Note::new(note.pitch, new_octave));
+        }
+
+        // 2. find thirds
+        loop {
+            if letters_added == LetterSet::FULL {
+                break;
+            }
+
+            let last = closed.last()
+                .expect("should be at least one elem");
+
+            let last_letter = last.pitch.letter();
+
+            let third_letter = Letter::from_step((last_letter.step() + 2) % 7)
+                .expect("should be valid letter");
+
+            let thirds = notes.extract_if(.., |n|
+                n.pitch.letter() == third_letter
+            );
+
+            let octave = match last_letter.cmp(&third_letter) {
+                Ordering::Less => last.octave,
+                Ordering::Greater => last.octave + 1,
+                Ordering::Equal => unreachable!("letter should be two steps away"),
+            };
+
+            let mut has_any = false;
+
+            for (i, note) in thirds.enumerate() {
+                has_any = true;
+
+                let octave = if separate_steps { octave + i as i16 } else { octave };
+
+                closed.push(Note::new(note.pitch, octave));
+            }
+
+            if has_any {
+                letters_added = letters_added.with_set(third_letter);
+            } else {
+                break;
+            }
+        }
+
+        // 3. insert leftovers
+
+        // to cluster notes with the same letter, sort them by letter
+        // which should also preserve ordering of accidentals
+        debug_assert!(
+            notes.is_sorted(),
+            "notes should be sorted (by octave)"
+        );
+
+        let (root_letter, root_octave) = {
+            let bass = *closed.first().expect("must have at least one note");
+
+            assert_eq!(
+                bass.pitch, self.root,
+                "before reordering, the bass should be the root",
+            );
+
+            (bass.pitch.letter(), bass.octave)
+        };
+
+        notes.sort_by_key(|n| root_letter.offset_between(n.pitch.letter()));
+
+        for notes in notes.chunk_by(|a, b| a.pitch.letter() == b.pitch.letter()) {
+            let letter = notes.first()
+                .expect("must be first item")
+                .pitch
+                .letter();
+
+            let octave = match root_letter.cmp(&letter) {
+                Ordering::Less => root_octave,
+                Ordering::Greater => root_octave + 1,
+                Ordering::Equal => unreachable!("letter should be root"),
+            };
+
+            for (i, note) in notes.iter().enumerate() {
+                let octave = if separate_steps { octave + i as i16 } else { octave };
+
+                closed.push(Note::new(note.pitch, octave));
+            }
+        }
+
+        // 4. inversion
+        // ensure state is right before reordering
+        if cfg!(debug_assertions) {
+            let bass_letter = self.bass().pitch.letter();
+
+            let first_bass = closed.iter().find(|n| n.pitch.letter() == bass_letter)
+                .expect("bass letter should exist in chord");
+
+            assert_eq!(
+                first_bass.pitch, self.bass().pitch,
+                "before reordering for inversion, make sure the right note is in the bass"
+            )
+        }
+
+        todo!()
     }
 
     pub fn transpose(&self, interval: Interval) -> Self {
