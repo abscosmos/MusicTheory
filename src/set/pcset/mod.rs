08351837838bb88643fa8efc4568ffc0a06c24f8
abscosmos <joshua@dinoslice.com>
2026-02-23 20:34:29 -0600

    Add method on PitchClass to check if inversionally symmetric

--- src/set/pcset/mod.rs
+++ src/set/pcset/mod.rs
@@ -411,6 +411,11 @@ impl PitchClassSet {
         Self(result).transpose(Semitones(2 * axis.chroma() as i16))
     }
 
+    pub const fn inversionally_symmetric(self) -> bool {
+        // FIXME(const): comparing by bits
+        self.invert_around(PitchClass::C).bits() == self.bits()
+    }
+
     /// Returns `true` if this set is a superset of the other set.
     ///
     /// A set is a superset of another if it contains all pitch classes in the other set.
