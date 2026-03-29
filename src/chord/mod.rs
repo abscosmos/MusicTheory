4054c23cc9a6070b6308e589e8f35394470549a7
abscosmos <joshua@dinoslice.com>
2026-03-29 17:26:54 -0500

    Publicly export chord types

--- src/chord/mod.rs
+++ src/chord/mod.rs
@@ -1,8 +1,13 @@
 pub mod shape;
 pub use shape::ChordShape;
 mod pitch;
+pub use pitch::PitchChord;
+
 mod note;
+pub use note::NoteChord;
+
+mod known;
+pub use known::KnownChord;
 
 pub(crate) mod root;
 mod letter_set;
-mod known;
